07ed832b18e8b8df3d3bb457b88125a458510475
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-09 23:01:19 +0700

    Update product-save.php

--- modules/product-save.php
+++ modules/product-save.php
@@ -8,9 +8,13 @@ add_action('admin_post_k86_save_product', 'k86_save_product');
 
 function k86_save_product() {
 
+    // Kiểm tra quyền
     if (!current_user_can('manage_options')) {
         wp_die('Bạn không có quyền.');
     }
+
+    // Kiểm tra Nonce chống CSRF
+    check_admin_referer('k86_save_product', 'k86_nonce');
 
     global $wpdb;
 
@@ -19,16 +23,28 @@ function k86_save_product() {
     $wpdb->insert(
         $table,
         array(
-            'name'        => sanitize_text_field($_POST['name']),
-            'slug'        => sanitize_title($_POST['name']),
-            'price'       => sanitize_text_field($_POST['price']),
+            'name'        => sanitize_text_field($_POST['name'] ?? ''),
+            'slug'        => sanitize_title($_POST['name'] ?? ''),
+            'price'       => sanitize_text_field($_POST['price'] ?? ''),
             'sale_price'  => '',
-            'shopee'      => esc_url_raw($_POST['shopee']),
-            'tiktok'      => esc_url_raw($_POST['tiktok']),
-            'lazada'      => esc_url_raw($_POST['lazada']),
+            'shopee'      => esc_url_raw($_POST['shopee'] ?? ''),
+            'tiktok'      => esc_url_raw($_POST['tiktok'] ?? ''),
+            'lazada'      => esc_url_raw($_POST['lazada'] ?? ''),
             'image'       => '',
-            'description' => sanitize_textarea_field($_POST['description']),
+            'description' => sanitize_textarea_field($_POST['description'] ?? ''),
             'status'      => 'active'
+        ),
+        array(
+            '%s',
+            '%s',
+            '%s',
+            '%s',
+            '%s',
+            '%s',
+            '%s',
+            '%s',
+            '%s',
+            '%s'
         )
     );
 
