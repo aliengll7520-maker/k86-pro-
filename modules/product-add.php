2ce56cc4ef8c29dbf8860ead8df4abb18341bb20
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-09 22:20:48 +0700

    Update product-add.php

--- modules/product-add.php
+++ modules/product-add.php
@@ -25,46 +25,58 @@ function k86_add_product_form() {
 
 <h1>Thêm sản phẩm Affiliate</h1>
 
-<form method="post">
+<form method="post" action="<?php echo admin_url('admin-post.php'); ?>">
+
+<input type="hidden" name="action" value="k86_save_product">
 
 <table class="form-table">
 
 <tr>
 <th>Tên sản phẩm</th>
-<td><input type="text" class="regular-text"></td>
+<td>
+    <input type="text" name="name" class="regular-text" required>
+</td>
 </tr>
 
 <tr>
 <th>Giá</th>
-<td><input type="text" class="regular-text"></td>
+<td>
+    <input type="text" name="price" class="regular-text">
+</td>
 </tr>
 
 <tr>
 <th>Link Shopee</th>
-<td><input type="url" class="regular-text"></td>
+<td>
+    <input type="url" name="shopee" class="regular-text">
+</td>
 </tr>
 
 <tr>
 <th>Link TikTok Shop</th>
-<td><input type="url" class="regular-text"></td>
+<td>
+    <input type="url" name="tiktok" class="regular-text">
+</td>
 </tr>
 
 <tr>
 <th>Link Lazada</th>
-<td><input type="url" class="regular-text"></td>
+<td>
+    <input type="url" name="lazada" class="regular-text">
+</td>
 </tr>
 
 <tr>
 <th>Mô tả</th>
-<td><textarea rows="5" class="large-text"></textarea></td>
+<td>
+    <textarea name="description" rows="5" class="large-text"></textarea>
+</td>
 </tr>
 
 </table>
 
 <p>
-<button class="button button-primary">
-Lưu sản phẩm
-</button>
+    <input type="submit" class="button button-primary" value="Lưu sản phẩm">
 </p>
 
 </form>
