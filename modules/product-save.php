53c9ce7627a8353288131effee62154635cee364
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-09 22:17:35 +0700

    Update product-save.php

--- modules/product-save.php
+++ modules/product-save.php
@@ -1,5 +1,37 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+add_action('admin_post_k86_save_product', 'k86_save_product');
+
+function k86_save_product() {
+
+    if (!current_user_can('manage_options')) {
+        wp_die('Bạn không có quyền.');
+    }
+
+    global $wpdb;
+
+    $table = $wpdb->prefix . 'k86_products';
+
+    $wpdb->insert(
+        $table,
+        array(
+            'name'        => sanitize_text_field($_POST['name']),
+            'slug'        => sanitize_title($_POST['name']),
+            'price'       => sanitize_text_field($_POST['price']),
+            'sale_price'  => '',
+            'shopee'      => esc_url_raw($_POST['shopee']),
+            'tiktok'      => esc_url_raw($_POST['tiktok']),
+            'lazada'      => esc_url_raw($_POST['lazada']),
+            'image'       => '',
+            'description' => sanitize_textarea_field($_POST['description']),
+            'status'      => 'active'
+        )
+    );
+
+    wp_redirect(admin_url('admin.php?page=k86-products'));
     exit;
 }
