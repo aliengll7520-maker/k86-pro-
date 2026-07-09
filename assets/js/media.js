7cfba150d5f0147eddfc1b1b87bb05fb5d006bb8
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-10 05:45:04 +0700

    Update media.js

--- assets/js/media.js
+++ assets/js/media.js
@@ -1,1 +1,27 @@
-// K86Shop Pro Media Library
+jQuery(document).ready(function ($) {
+
+    $('.k86-upload-image').on('click', function (e) {
+
+        e.preventDefault();
+
+        var frame = wp.media({
+            title: 'Chọn ảnh sản phẩm',
+            button: {
+                text: 'Sử dụng ảnh này'
+            },
+            multiple: false
+        });
+
+        frame.on('select', function () {
+
+            var attachment = frame.state().get('selection').first().toJSON();
+
+            $('#image').val(attachment.url);
+
+        });
+
+        frame.open();
+
+    });
+
+});
