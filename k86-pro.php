5d9ebe44b4a28fb5593557b3bde1c6993f1155c4
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-09 18:33:09 +0700

    Create k86-pro.php

---
+++ k86-pro.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: K86 Pro
+ * Plugin URI: https://k86shop.com
+ * Description: Plugin hỗ trợ Affiliate và SEO cho K86Shop.
+ * Version: 1.0.0
+ * Author: Liểng Sang
+ * License: GPL2
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
