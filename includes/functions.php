116a432dd17508ab4dd2c6f37258e849b93784b4
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-09 19:25:03 +0700

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -10,3 +10,13 @@ function k86_pro_version() {
 function k86_pro_name() {
     return 'K86 Pro';
 }
+function k86_pro_enqueue_style() {
+    wp_enqueue_style(
+        'k86-pro-style',
+        plugin_dir_url(dirname(__FILE__)) . 'assets/style.css',
+        array(),
+        k86_pro_version()
+    );
+}
+
+add_action('wp_enqueue_scripts', 'k86_pro_enqueue_style');
