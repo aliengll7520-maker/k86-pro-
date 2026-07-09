2e5188a31f98f7ece1dc585b6fa681b3caf428da
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-09 18:39:21 +0700

    Update k86-pro.php

--- k86-pro.php
+++ k86-pro.php
@@ -1,13 +1,13 @@
-<?php
+ <?php
 /**
  * Plugin Name: K86 Pro
- * Plugin URI: https://k86shop.com
- * Description: Plugin hỗ trợ Affiliate và SEO cho K86Shop.
+ * Plugin URI: https://github.com/aliengll7520-maker/k86-pro
+ * Description: Plugin Affiliate dành cho K86Shop.
  * Version: 1.0.0
  * Author: Liểng Sang
  * License: GPL2
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit;
 }
