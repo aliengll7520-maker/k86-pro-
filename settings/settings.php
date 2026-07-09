c88e04e6b3696955c21ab2096d6ab8d743147b82
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-09 19:54:44 +0700

    Update settings.php

--- settings/settings.php
+++ settings/settings.php
@@ -1,5 +1,29 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit;
+}
+
+add_action('admin_menu', 'k86_pro_settings_menu');
+
+function k86_pro_settings_menu() {
+
+    add_submenu_page(
+        'k86-pro',
+        'Cài đặt',
+        'Cài đặt',
+        'manage_options',
+        'k86-settings',
+        'k86_settings_page'
+    );
+
+}
+
+function k86_settings_page() {
+    ?>
+    <div class="wrap">
+        <h1>Cài đặt K86 Pro</h1>
+        <p>Trang cài đặt sẽ được phát triển ở các bước tiếp theo.</p>
+    </div>
+    <?php
 }
