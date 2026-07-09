78ef06578e38b0247c1d930c65f123a7b89ebd52
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-09 20:45:54 +0700

    Create      product-manager.php

---
+++ modules/product-manager.php
@@ -0,0 +1,54 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+add_action('admin_menu', 'k86_product_menu');
+
+function k86_product_menu() {
+
+    add_submenu_page(
+        'k86-pro',
+        'Sản phẩm',
+        'Sản phẩm',
+        'manage_options',
+        'k86-products',
+        'k86_products_page'
+    );
+
+}
+
+function k86_products_page() {
+?>
+
+<div class="wrap">
+
+    <h1>Quản lý sản phẩm</h1>
+
+    <p>Đây là nơi quản lý các sản phẩm Affiliate của K86 Pro.</p>
+
+    <table class="widefat striped">
+
+        <thead>
+            <tr>
+                <th>Tên sản phẩm</th>
+                <th>Giá</th>
+                <th>Trạng thái</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td>Camera Wifi TP-Link Tapo C220</td>
+                <td>699.000đ</td>
+                <td>Hoạt động</td>
+            </tr>
+        </tbody>
+
+    </table>
+
+</div>
+
+<?php
+}
