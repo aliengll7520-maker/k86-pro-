bb83e3d3f824526f2863980a15115fe4d2ab6707
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-12 21:34:23 +0700

    Update version.php

--- core/version.php
+++ core/version.php
@@ -3,7 +3,8 @@
  * --------------------------------------------------------
  * K86 Pro
  * Core: Version Manager
- * Version: 1.0.0
+ * Version: 1.5.1
+ * Status: Development
  * --------------------------------------------------------
  */
 
@@ -12,9 +13,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
+ * --------------------------------------------------------
  * Phiên bản Database của K86 Pro
+ * --------------------------------------------------------
  *
- * Mỗi khi thay đổi cấu trúc cơ sở dữ liệu,
- * chỉ cần tăng số phiên bản này.
+ * Chỉ tăng phiên bản khi có thay đổi cấu trúc Database.
+ *
+ * Ví dụ:
+ * - Thêm cột mới
+ * - Xóa cột
+ * - Đổi kiểu dữ liệu
+ * - Thêm bảng mới
+ *
+ * Không tăng phiên bản khi chỉ sửa giao diện
+ * hoặc chỉnh sửa mã nguồn.
  */
-define( 'K86_DB_VERSION', '1.0.0' );
+define( 'K86_DB_VERSION', '1.1.0' );
