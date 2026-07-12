7ff2a1ca09b7874ba9b327c4cc6a867a9666e925
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-12 18:52:56 +0700

    Add Core Version Manager

---
+++ core/version.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * --------------------------------------------------------
+ * K86 Pro
+ * Core: Version Manager
+ * Version: 1.0.0
+ * --------------------------------------------------------
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Phiên bản Database của K86 Pro
+ *
+ * Mỗi khi thay đổi cấu trúc cơ sở dữ liệu,
+ * chỉ cần tăng số phiên bản này.
+ */
+define( 'K86_DB_VERSION', '1.0.0' );
