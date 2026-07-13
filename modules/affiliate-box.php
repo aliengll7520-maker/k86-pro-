346e819f4d01e0625611b4a23aea1c67d3a8fb5c
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-13 21:17:26 +0700

    Update affiliate-box.php

--- modules/affiliate-box.php
+++ modules/affiliate-box.php
@@ -1,33 +1,64 @@
 <?php
+/**
+ * --------------------------------------------------------
+ * K86 Pro
+ * Module: Affiliate Box
+ * Version: 1.5.2
+ * Status: Framework RC1
+ * --------------------------------------------------------
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
-function k86_affiliate_box() {
-
-    ob_start();
-    ?>
-
-    <div class="k86-box">
-        <h3 class="k86-title">Camera Wifi TP-Link Tapo C220</h3>
-
-        <p>
-            Camera AI 4MP, quay 360°, đàm thoại 2 chiều,
-            phát hiện chuyển động thông minh.
-        </p>
-
-        <div class="k86-price">
-            Giá tham khảo: <strong>699.000đ</strong>
-        </div>
-
-        <a class="k86-buy" href="#" target="_blank">
-            🛒 Mua ngay
-        </a>
-    </div>
-
-    <?php
-    return ob_get_clean();
-}
-
+/**
+ * Đăng ký Shortcode Affiliate Box.
+ */
 add_shortcode( 'k86_box', 'k86_affiliate_box' );
+
+/**
+ * Hiển thị Affiliate Box.
+ *
+ * Wrapper để tương thích với các phiên bản cũ.
+ *
+ * Ví dụ:
+ * [k86_box id="1"]
+ *
+ * @param array $atts Thuộc tính Shortcode.
+ * @return string
+ */
+function k86_affiliate_box( $atts ) {
+
+	/**
+	 * Cho phép các Module mở rộng xử lý trước.
+	 */
+	do_action( 'k86_affiliate_box_before', $atts );
+
+	/**
+	 * Chuyển tiếp sang Product Shortcode.
+	 *
+	 * Điều này giúp chỉ còn một nơi duy nhất
+	 * quản lý giao diện Product Box.
+	 */
+	$output = k86_product_shortcode( $atts );
+
+	/**
+	 * Cho phép thay đổi HTML trước khi trả về.
+	 */
+	$output = apply_filters(
+		'k86_affiliate_box_output',
+		$output,
+		$atts
+	);
+
+	/**
+	 * Hook sau khi hiển thị.
+	 */
+	do_action(
+		'k86_affiliate_box_after',
+		$atts
+	);
+
+	return $output;
+}
