8fb8154baa3ce424357fa5635a71b1e91edcbd34
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-16 03:15:45 +0700

    Create decision-render.php

---
+++ modules/decision-render.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Decision Render
+ * Module: K86 Pro
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+function k86_render_decision_box( $product ) {
+
+    if ( empty( $product ) ) {
+        return '';
+    }
+
+    return '';
+
+}
