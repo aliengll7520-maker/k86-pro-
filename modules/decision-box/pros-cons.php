9530c30bce69379403321fb91571451cfd1da22b
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-16 03:00:12 +0700

    Update pros-cons.php

--- modules/decision-box/pros-cons.php
+++ modules/decision-box/pros-cons.php
@@ -1,1 +1,37 @@
 <?php
+/**
+ * K86 Smart Decision Box
+ * Pros & Cons Template
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+?>
+
+<div class="k86-pros-cons">
+
+    <div class="k86-pros">
+
+        <h3>Ưu điểm</h3>
+
+        <ul>
+            <li>Dễ sử dụng.</li>
+            <li>Hiệu năng tốt.</li>
+            <li>Giá thành hợp lý.</li>
+        </ul>
+
+    </div>
+
+    <div class="k86-cons">
+
+        <h3>Nhược điểm</h3>
+
+        <ul>
+            <li>Ít tùy chọn nâng cao.</li>
+            <li>Không phù hợp với mọi nhu cầu.</li>
+        </ul>
+
+    </div>
+
+</div>
