957d35f289948e24cc9b633d817d4d4d2954e0f1
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-16 03:01:05 +0700

    Update cta.php

--- modules/decision-box/cta.php
+++ modules/decision-box/cta.php
@@ -1,1 +1,22 @@
 <?php
+/**
+ * K86 Smart Decision Box
+ * CTA Template
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+?>
+
+<div class="k86-decision-cta">
+
+    <a href="#" class="k86-btn-primary">
+        Xem giá mới nhất
+    </a>
+
+    <a href="#" class="k86-btn-secondary">
+        Xem đánh giá chi tiết
+    </a>
+
+</div>
