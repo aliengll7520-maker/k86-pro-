12564c373fb663a88ac616271c5802ee1e5f1c7e
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-16 04:01:26 +0700

    Build Decision Render Engine

--- modules/decision-render.php
+++ modules/decision-render.php
@@ -8,12 +8,47 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+/**
+ * Hiển thị Decision Box.
+ *
+ * @param object $product
+ * @return string
+ */
 function k86_render_decision_box( $product ) {
 
     if ( empty( $product ) ) {
         return '';
     }
 
-    return '';
+    if ( ! function_exists( 'k86_decision_box_enabled' ) ) {
+        return '';
+    }
 
+    if ( ! k86_decision_box_enabled() ) {
+        return '';
+    }
+
+    ob_start();
+
+    $template_path = K86_PRO_MODULES . 'decision-box/';
+
+    $templates = array(
+        'header.php',
+        'content.php',
+        'pros-cons.php',
+        'cta.php',
+        'footer.php',
+    );
+
+    foreach ( $templates as $template ) {
+
+        $file = $template_path . $template;
+
+        if ( file_exists( $file ) ) {
+            require $file;
+        }
+
+    }
+
+    return ob_get_clean();
 }
