d8ae212a6dffffb5c7847810e2b20226c483d8df
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-16 03:02:50 +0700

    Update footer.php

--- modules/decision-box/footer.php
+++ modules/decision-box/footer.php
@@ -1,1 +1,26 @@
 <?php
+/**
+ * K86 Smart Decision Box
+ * Footer Template
+ *
+ * @package K86_Pro
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+?>
+
+<div class="k86-decision-footer">
+
+    <div class="k86-decision-note">
+
+        <small>
+            Thông tin chỉ mang tính tham khảo. Hãy cân nhắc nhu cầu thực tế trước khi quyết định mua sản phẩm.
+        </small>
+
+    </div>
+
+</div>
+
+</div><!-- /.k86-decision-box -->
