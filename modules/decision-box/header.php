d1e8004664548242b2de0eada9783492b33e72c4
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-16 02:56:42 +0700

    Update header.php

--- modules/decision-box/header.php
+++ modules/decision-box/header.php
@@ -1,1 +1,28 @@
 <?php
+/**
+ * K86 Smart Decision Box
+ * Header Template
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+?>
+
+<div class="k86-decision-box">
+
+    <div class="k86-decision-header">
+
+        <span class="k86-badge">
+            K86 Smart Decision
+        </span>
+
+        <h2 class="k86-title">
+            Có nên mua sản phẩm này?
+        </h2>
+
+        <p class="k86-subtitle">
+            Phân tích nhanh ưu điểm, nhược điểm và đưa ra gợi ý phù hợp.
+        </p>
+
+    </div>
