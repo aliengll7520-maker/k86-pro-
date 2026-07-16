0a3ddfe15bdb3436b8155bbd127a1fb1710df3d7
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-16 13:57:51 +0700

    Create Business Loader

---
+++ business/loader.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * ------------------------------------------------------------------------
+ * K86 Pro
+ * Business Loader
+ * ------------------------------------------------------------------------
+ *
+ * Nạp toàn bộ Business Engine.
+ *
+ * @package K86_Pro
+ * @since 1.6.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Business Ready.
+ */
+do_action( 'k86_business_ready' );
