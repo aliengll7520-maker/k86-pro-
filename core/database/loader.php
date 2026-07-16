700102d127fe959811b5ee8be93395b053b8f9d8
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-16 21:30:59 +0700

    Update loader.php

--- core/database/loader.php
+++ core/database/loader.php
@@ -1,1 +1,16 @@
+<?php
+/**
+ * Database Engine Loader
+ *
+ * @package K86_Pro
+ */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require_once __DIR__ . '/database.php';
+require_once __DIR__ . '/install.php';
+require_once __DIR__ . '/installer.php';
+require_once __DIR__ . '/update.php';
+require_once __DIR__ . '/upgrade.php';
