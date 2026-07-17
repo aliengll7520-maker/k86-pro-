00785554ea053b507cdc20ae87efb08c653c16af
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-17 21:48:03 +0700

    Update loader.php

--- business/product/loader.php
+++ business/product/loader.php
@@ -1,15 +1,1 @@
-<?php
-/**
- * Product Model
- *
- * Định nghĩa cấu trúc dữ liệu sản phẩm.
- *
- * @package K86_Pro
- * @since 2.0.0
- */
 
-defined( 'ABSPATH' ) || exit;
-/**
- * Product Model
- */
-require_once __DIR__ . '/model.php';
