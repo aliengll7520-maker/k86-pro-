482b588bed6064bb9ae0c5926e204d131ee8a038
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-19 05:13:13 +0700

    Update engagement.js

--- assets/js/engagement.js
+++ assets/js/engagement.js
@@ -10,6 +10,240 @@
 
 	'use strict';
 
-	console.log('K86 Engagement Engine Loaded');
+	window.K86Engagement = window.K86Engagement || {};
+
+	K86Engagement.version = '1.0.0';
+
+	K86Engagement.init = function () {
+
+		this.bindReactions();
+
+		this.bindShare();
+
+		this.bindCopy();
+
+		console.log('K86 Engagement Engine Loaded');
+
+	};
+
+	/*
+	|--------------------------------------------------------------------------
+	| Reaction
+	|--------------------------------------------------------------------------
+	*/
+
+	K86Engagement.bindReactions = function () {
+
+		$(document).on(
+			'click',
+			'.k86-reaction-button',
+			function (e) {
+
+				e.preventDefault();
+
+				const button = $(this);
+
+				const reaction = button.data('reaction');
+
+				$('.k86-reaction-button')
+					.removeClass('is-active');
+
+				button.addClass('is-active');
+
+				K86Engagement.sendReaction(
+					reaction
+				);
+
+			}
+		);
+
+	};
+
+	K86Engagement.sendReaction = function (reaction) {
+
+		console.log(
+			'Reaction:',
+			reaction
+		);
+
+		/*
+		 * AJAX sẽ bổ sung
+		 * ở bước includes/ajax.php
+		 */
+
+	};
+		/*
+	|--------------------------------------------------------------------------
+	| Share
+	|--------------------------------------------------------------------------
+	*/
+
+	K86Engagement.bindShare = function () {
+
+		$(document).on(
+			'click',
+			'.k86-share-button',
+			function (e) {
+
+				e.preventDefault();
+
+				const platform = $(this).data('platform');
+
+				K86Engagement.share(platform);
+
+			}
+		);
+
+	};
+
+	K86Engagement.share = function (platform) {
+
+		const url = encodeURIComponent(
+			window.location.href
+		);
+
+		switch ( platform ) {
+
+			case 'facebook':
+
+				window.open(
+					'https://www.facebook.com/sharer/sharer.php?u=' + url,
+					'_blank'
+				);
+
+				break;
+
+			case 'x':
+
+				window.open(
+					'https://twitter.com/intent/tweet?url=' + url,
+					'_blank'
+				);
+
+				break;
+
+			case 'telegram':
+
+				window.open(
+					'https://t.me/share/url?url=' + url,
+					'_blank'
+				);
+
+				break;
+
+			default:
+
+				if ( navigator.share ) {
+
+					navigator.share({
+
+						url: window.location.href
+
+					});
+
+				}
+
+		}
+
+	};
+
+	/*
+	|--------------------------------------------------------------------------
+	| AJAX Framework
+	|--------------------------------------------------------------------------
+	*/
+
+	K86Engagement.ajax = function ( action, data ) {
+
+		if (
+			typeof k86_ajax === 'undefined'
+		) {
+			return;
+		}
+
+		return $.ajax({
+
+			url: k86_ajax.ajax_url,
+
+			type: 'POST',
+
+			data: $.extend({
+
+				action: action,
+
+				nonce: k86_ajax.nonce
+
+			}, data )
+
+		});
+
+	};
+
+	/*
+	|--------------------------------------------------------------------------
+	| Boot Engine
+	|--------------------------------------------------------------------------
+	*/
+
+	$(function () {
+
+		K86Engagement.init();
+
+	});
 
 })(jQuery);
+
+	/*
+	|--------------------------------------------------------------------------
+	| Copy Link
+	|--------------------------------------------------------------------------
+	*/
+
+	K86Engagement.bindCopy = function () {
+
+		$(document).on(
+			'click',
+			'.k86-copy-button',
+			function (e) {
+
+				e.preventDefault();
+
+				K86Engagement.copyLink();
+
+			}
+		);
+
+	};
+
+	K86Engagement.copyLink = function () {
+
+		const url = window.location.href;
+
+		if (
+			navigator.clipboard &&
+			navigator.clipboard.writeText
+		) {
+
+			navigator.clipboard.writeText(url);
+
+			alert('Đã sao chép liên kết.');
+
+			return;
+
+		}
+
+		const input = document.createElement('input');
+
+		input.value = url;
+
+		document.body.appendChild(input);
+
+		input.select();
+
+		document.execCommand('copy');
+
+		document.body.removeChild(input);
+
+		alert('Đã sao chép liên kết.');
+
+	};
