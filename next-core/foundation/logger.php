4bb64b5901fcdcd559f2433877bde45607bbe9b6
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-20 12:22:56 +0700

    Update logger.php

--- next-core/foundation/logger.php
+++ next-core/foundation/logger.php
@@ -1,8 +1,49 @@
 <?php
 /**
- * Logger
+ * K86 Pro Next Core
  *
- * Ghi nhật ký hoạt động của Foundation.
+ * Foundation Logger
+ *
+ * Chịu trách nhiệm ghi log cho toàn bộ Framework.
+ *
+ * @package K86Pro
  */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'K86_Logger' ) ) {
+
+	class K86_Logger {
+
+		/**
+		 * Khởi tạo Logger.
+		 *
+		 * @return void
+		 */
+		public function init() {
+
+		}
+
+		/**
+		 * Ghi log.
+		 *
+		 * @param mixed  $data
+		 * @param string $level
+		 * @return void
+		 */
+		public function log( $data, $level = 'info' ) {
+
+		}
+
+		/**
+		 * Xóa log.
+		 *
+		 * @return void
+		 */
+		public function clear() {
+
+		}
+
+	}
+
+}
