154de876e83d372d63abe280a42bd9608f3ab5eb
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-20 13:04:53 +0700

    Update loader.php

--- next-core/foundation/loader.php
+++ next-core/foundation/loader.php
@@ -1,8 +1,68 @@
 <?php
 /**
- * Loader
+ * K86 Pro Next Core
  *
- * Quản lý việc nạp các thành phần của Foundation.
+ * Foundation Loader
+ *
+ * Quản lý việc nạp và khởi tạo các thành phần của Framework.
+ *
+ * @package K86Pro
  */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'K86_Loader' ) ) {
+
+	class K86_Loader {
+
+		/**
+		 * Danh sách thành phần cần nạp.
+		 *
+		 * @var array
+		 */
+		protected $components = array();
+
+		/**
+		 * Khởi tạo Loader.
+		 *
+		 * @return void
+		 */
+		public function init() {
+
+			$this->components = array();
+
+		}
+
+		/**
+		 * Đăng ký thành phần.
+		 *
+		 * @param string $key
+		 * @param mixed  $component
+		 * @return void
+		 */
+		public function register( $key, $component ) {
+
+			$this->components[ $key ] = $component;
+
+		}
+
+		/**
+		 * Nạp tất cả thành phần đã đăng ký.
+		 *
+		 * @return void
+		 */
+		public function load() {
+
+			foreach ( $this->components as $component ) {
+
+				if ( is_object( $component ) && method_exists( $component, 'init' ) ) {
+					$component->init();
+				}
+
+			}
+
+		}
+
+	}
+
+}
