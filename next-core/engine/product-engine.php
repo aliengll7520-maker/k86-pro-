ed494af2d4702501123235073708708dc3b1044e
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-20 13:11:32 +0700

    Update product-engine.php

--- next-core/engine/product-engine.php
+++ next-core/engine/product-engine.php
@@ -1,8 +1,68 @@
 <?php
 /**
+ * K86 Pro Next Core
+ *
  * Product Engine
  *
- * Quản lý toàn bộ dữ liệu và xử lý sản phẩm.
+ * Engine quản lý sản phẩm của Framework.
+ *
+ * @package K86Pro
  */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'K86_Product_Engine' ) ) {
+
+	class K86_Product_Engine {
+
+		/**
+		 * Danh sách sản phẩm.
+		 *
+		 * @var array
+		 */
+		protected $products = array();
+
+		/**
+		 * Khởi tạo Product Engine.
+		 *
+		 * @return void
+		 */
+		public function init() {
+
+			$this->products = array();
+
+		}
+
+		/**
+		 * Đăng ký sản phẩm.
+		 *
+		 * @param string $key
+		 * @param mixed  $product
+		 * @return void
+		 */
+		public function register( $key, $product ) {
+
+			$this->products[ $key ] = $product;
+
+		}
+
+		/**
+		 * Lấy sản phẩm.
+		 *
+		 * @param string $key
+		 * @param mixed  $default
+		 * @return mixed
+		 */
+		public function get( $key, $default = null ) {
+
+			if ( array_key_exists( $key, $this->products ) ) {
+				return $this->products[ $key ];
+			}
+
+			return $default;
+
+		}
+
+	}
+
+}
