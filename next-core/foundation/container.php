fbedf7963236c124b63b33e56b11a6ab3683ad2f
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-20 19:09:31 +0700

    Update container.php

--- next-core/foundation/container.php
+++ next-core/foundation/container.php
@@ -1,67 +1,121 @@
 <?php
 /**
  * K86 Pro Next Core
- *
- * Foundation Container
- *
- * Quản lý các service của Framework.
+ * Container
  *
  * @package K86Pro
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-if ( ! class_exists( 'K86_Container' ) ) {
+if (!class_exists('K86_Container')) {
 
-	class K86_Container {
+    class K86_Container {
 
-		/**
-		 * Danh sách service.
-		 *
-		 * @var array
-		 */
-		protected $services = array();
+        /**
+         * Danh sách factory.
+         *
+         * @var array
+         */
+        protected $bindings = array();
 
-		/**
-		 * Khởi tạo Container.
-		 *
-		 * @return void
-		 */
-		public function init() {
+        /**
+         * Danh sách instance.
+         *
+         * @var array
+         */
+        protected $instances = array();
 
-			$this->services = array();
+        /**
+         * Đăng ký factory.
+         *
+         * @param string   $key
+         * @param callable $factory
+         * @return $this
+         */
+        public function bind($key, callable $factory) {
 
-		}
+            $this->bindings[$key] = $factory;
 
-		/**
-		 * Đăng ký service.
-		 *
-		 * @param string $key
-		 * @param mixed  $service
-		 * @return void
-		 */
-		public function set( $key, $service ) {
+            return $this;
+        }
 
-			$this->services[ $key ] = $service;
+        /**
+         * Đăng ký instance có sẵn.
+         *
+         * @param string $key
+         * @param mixed  $instance
+         * @return $this
+         */
+        public function instance($key, $instance) {
 
-		}
+            $this->instances[$key] = $instance;
 
-		/**
-		 * Lấy service.
-		 *
-		 * @param string $key
-		 * @param mixed  $default
-		 * @return mixed
-		 */
-		public function get( $key, $default = null ) {
+            return $this;
+        }
 
-			if ( array_key_exists( $key, $this->services ) ) {
-				return $this->services[ $key ];
-			}
+        /**
+         * Lấy đối tượng.
+         *
+         * @param string $key
+         * @return mixed|null
+         */
+        public function get($key) {
 
-			return $default;
+            if (isset($this->instances[$key])) {
+                return $this->instances[$key];
+            }
 
-		}
+            if (!isset($this->bindings[$key])) {
+                return null;
+            }
 
-	}
+            $this->instances[$key] = call_user_func(
+                $this->bindings[$key],
+                $this
+            );
+
+            return $this->instances[$key];
+        }
+
+        /**
+         * Kiểm tra tồn tại.
+         *
+         * @param string $key
+         * @return bool
+         */
+        public function has($key) {
+
+            return isset($this->bindings[$key])
+                || isset($this->instances[$key]);
+        }
+
+        /**
+         * Xóa binding và instance.
+         *
+         * @param string $key
+         * @return bool
+         */
+        public function remove($key) {
+
+            unset($this->bindings[$key]);
+            unset($this->instances[$key]);
+
+            return true;
+        }
+
+        /**
+         * Xóa toàn bộ.
+         *
+         * @return $this
+         */
+        public function clear() {
+
+            $this->bindings = array();
+            $this->instances = array();
+
+            return $this;
+        }
+    }
+
 }
