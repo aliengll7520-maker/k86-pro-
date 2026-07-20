03dc0b6ebb343e2846e9f2d21552cd33baf2ca49
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-20 20:46:33 +0700

    Update product-renderer.php

--- next-core/render/product-renderer.php
+++ next-core/render/product-renderer.php
@@ -1,1 +1,62 @@
+<?php
+/**
+ * K86 Pro Next Core
+ * Product Renderer
+ *
+ * @package K86Pro
+ */
 
+defined('ABSPATH') || exit;
+
+if (!class_exists('K86_Product_Renderer')) {
+
+    class K86_Product_Renderer {
+
+        /**
+         * Product Service.
+         *
+         * @var K86_Product_Service
+         */
+        protected $service;
+
+        /**
+         * Constructor.
+         *
+         * @param K86_Product_Service $service
+         */
+        public function __construct($service) {
+            $this->service = $service;
+        }
+
+        /**
+         * Render product.
+         *
+         * @return string
+         */
+        public function render() {
+
+            $price = $this->service->price();
+            $stock = $this->service->in_stock();
+
+            ob_start();
+            ?>
+
+            <div class="k86-product-box">
+
+                <div class="k86-product-price">
+                    <?php echo esc_html($price); ?>
+                </div>
+
+                <div class="k86-product-stock">
+                    <?php echo $stock ? 'Còn hàng' : 'Hết hàng'; ?>
+                </div>
+
+            </div>
+
+            <?php
+
+            return ob_get_clean();
+        }
+    }
+
+}
