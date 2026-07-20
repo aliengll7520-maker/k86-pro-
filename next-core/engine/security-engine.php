42ad41522752885ac6c80eab1fbd09b4f0423df5
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-20 13:53:20 +0700

    Create security-engine.php

---
+++ next-core/engine/security-engine.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * K86 Pro Next Core
+ *
+ * Security Engine
+ *
+ * Engine quản lý bảo mật hệ thống.
+ *
+ * @package K86Pro
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'K86_Security_Engine' ) ) {
+
+	class K86_Security_Engine {
+
+		/**
+		 * Danh sách security handlers.
+		 *
+		 * @var array
+		 */
+		protected $handlers = array();
+
+		/**
+		 * Khởi tạo Engine.
+		 *
+		 * @return void
+		 */
+		public function init() {
+
+			$this->handlers = array();
+
+		}
+
+		/**
+		 * Đăng ký handler.
+		 *
+		 * @param string $key
+		 * @param mixed  $handler
+		 * @return void
+		 */
+		public function register( $key, $handler ) {
+
+			$this->handlers[ $key ] = $handler;
+
+		}
+
+		/**
+		 * Lấy handler.
+		 *
+		 * @param string $key
+		 * @param mixed  $default
+		 * @return mixed
+		 */
+		public function get( $key, $default = null ) {
+
+			if ( array_key_exists( $key, $this->handlers ) ) {
+				return $this->handlers[ $key ];
+			}
+
+			return $default;
+
+		}
+
+	}
+
+}
