57f1b2c26c37ad4bb944bd2b47a798f29ba76503
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-20 19:22:56 +0700

    Update bootstrap.php

--- next-core/foundation/bootstrap.php
+++ next-core/foundation/bootstrap.php
@@ -1,63 +1,114 @@
 <?php
 /**
  * K86 Pro Next Core
- *
- * Foundation Bootstrap
- *
- * Khởi tạo toàn bộ Foundation của Framework.
+ * Bootstrap
  *
  * @package K86Pro
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-if ( ! class_exists( 'K86_Bootstrap' ) ) {
+if (!class_exists('K86_Bootstrap')) {
 
-	class K86_Bootstrap {
+    class K86_Bootstrap {
 
-		/**
-		 * Loader.
-		 *
-		 * @var K86_Loader|null
-		 */
-		protected $loader = null;
+        /**
+         * Registry.
+         *
+         * @var K86_Registry
+         */
+        protected $registry;
 
-		/**
-		 * Khởi tạo Bootstrap.
-		 *
-		 * @return void
-		 */
-		public function init() {
+        /**
+         * Container.
+         *
+         * @var K86_Container
+         */
+        protected $container;
 
-			$this->loader = null;
+        /**
+         * Engine Manager.
+         *
+         * @var K86_Engine_Manager
+         */
+        protected $engine_manager;
 
-		}
+        /**
+         * Khởi động Next Core.
+         */
+        public function boot() {
 
-		/**
-		 * Thiết lập Loader.
-		 *
-		 * @param K86_Loader $loader Đối tượng Loader.
-		 * @return void
-		 */
-		public function set_loader( $loader ) {
+            // Nạp toàn bộ class
+            $loader = new K86_Loader();
+            $loader->load();
 
-			$this->loader = $loader;
+            // Registry
+            $this->registry = new K86_Registry();
 
-		}
+            // Container
+            $this->container = new K86_Container();
 
-		/**
-		 * Khởi động Foundation.
-		 *
-		 * @return void
-		 */
-		public function boot() {
+            // Engine Manager
+            $this->engine_manager = new K86_Engine_Manager();
 
-			if ( $this->loader instanceof K86_Loader ) {
-				$this->loader->load();
-			}
+            // Đăng ký Engine
+            $this->engine_manager->register(
+                'product',
+                new K86_Product_Engine()
+            );
 
-		}
+            $this->engine_manager->register(
+                'media',
+                new K86_Media_Engine()
+            );
 
-	}
+            $this->engine_manager->register(
+                'pricing',
+                new K86_Pricing_Engine()
+            );
+
+            $this->engine_manager->register(
+                'review',
+                new K86_Review_Engine()
+            );
+
+            $this->engine_manager->register(
+                'inventory',
+                new K86_Inventory_Engine()
+            );
+
+            $this->engine_manager->register(
+                'voucher',
+                new K86_Voucher_Engine()
+            );
+
+            // Đưa vào Registry
+            $this->registry->set('container', $this->container);
+            $this->registry->set('engine_manager', $this->engine_manager);
+
+            return $this;
+        }
+
+        /**
+         * Lấy Registry.
+         */
+        public function registry() {
+            return $this->registry;
+        }
+
+        /**
+         * Lấy Container.
+         */
+        public function container() {
+            return $this->container;
+        }
+
+        /**
+         * Lấy Engine Manager.
+         */
+        public function engine_manager() {
+            return $this->engine_manager;
+        }
+    }
 
 }
