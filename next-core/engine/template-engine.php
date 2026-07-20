89f9b30b0518728ee66fb1080fcab8fcb4ada103
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-20 14:28:13 +0700

    Update template-engine.php

--- next-core/engine/template-engine.php
+++ next-core/engine/template-engine.php
@@ -1,8 +1,68 @@
 <?php
 /**
+ * K86 Pro Next Core
+ *
  * Template Engine
  *
- * Quản lý template của plugin.
+ * Engine quản lý Template của hệ thống.
+ *
+ * @package K86Pro
  */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'K86_Template_Engine' ) ) {
+
+	class K86_Template_Engine {
+
+		/**
+		 * Danh sách template handlers.
+		 *
+		 * @var array
+		 */
+		protected $handlers = array();
+
+		/**
+		 * Khởi tạo Engine.
+		 *
+		 * @return void
+		 */
+		public function init() {
+
+			$this->handlers = array();
+
+		}
+
+		/**
+		 * Đăng ký template.
+		 *
+		 * @param string $key
+		 * @param mixed  $handler
+		 * @return void
+		 */
+		public function register( $key, $handler ) {
+
+			$this->handlers[ $key ] = $handler;
+
+		}
+
+		/**
+		 * Lấy template.
+		 *
+		 * @param string $key
+		 * @param mixed  $default
+		 * @return mixed
+		 */
+		public function get( $key, $default = null ) {
+
+			if ( array_key_exists( $key, $this->handlers ) ) {
+				return $this->handlers[ $key ];
+			}
+
+			return $default;
+
+		}
+
+	}
+
+}
