2b98621239cf65b88c1ac8fc28c0648df42056ff
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-20 12:16:35 +0700

    Update error-handler.php

--- next-core/foundation/error-handler.php
+++ next-core/foundation/error-handler.php
@@ -9,7 +9,7 @@
  * @package K86Pro
  */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
 if ( ! class_exists( 'K86_Error_Handler' ) ) {
 
@@ -21,6 +21,39 @@ if ( ! class_exists( 'K86_Error_Handler' ) ) {
 		 * @return void
 		 */
 		public function init() {
+
+		}
+
+		/**
+		 * Xử lý Exception.
+		 *
+		 * @param \Throwable $exception
+		 * @return void
+		 */
+		public function handle_exception( $exception ) {
+
+		}
+
+		/**
+		 * Xử lý Error.
+		 *
+		 * @param int    $errno
+		 * @param string $errstr
+		 * @param string $errfile
+		 * @param int    $errline
+		 * @return void
+		 */
+		public function handle_error( $errno, $errstr, $errfile, $errline ) {
+
+		}
+
+		/**
+		 * Ghi log lỗi.
+		 *
+		 * @param mixed $data
+		 * @return void
+		 */
+		protected function write_log( $data ) {
 
 		}
 
