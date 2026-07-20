b899466497f153c61783f0a24141b3d545bb04e6
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-20 14:19:33 +0700

    Update module-engine.php

--- next-core/engine/module-engine.php
+++ next-core/engine/module-engine.php
@@ -1,8 +1,68 @@
 <?php
 /**
+ * K86 Pro Next Core
+ *
  * Module Engine
  *
- * Quản lý các module độc lập.
+ * Engine quản lý các Module của hệ thống.
+ *
+ * @package K86Pro
  */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'K86_Module_Engine' ) ) {
+
+	class K86_Module_Engine {
+
+		/**
+		 * Danh sách module handlers.
+		 *
+		 * @var array
+		 */
+		protected $handlers = array();
+
+		/**
+		 * Khởi tạo Engine.
+		 *
+		 * @return void
+		 */
+		public function init() {
+
+			$this->handlers = array();
+
+		}
+
+		/**
+		 * Đăng ký module.
+		 *
+		 * @param string $key
+		 * @param mixed  $handler
+		 * @return void
+		 */
+		public function register( $key, $handler ) {
+
+			$this->handlers[ $key ] = $handler;
+
+		}
+
+		/**
+		 * Lấy module.
+		 *
+		 * @param string $key
+		 * @param mixed  $default
+		 * @return mixed
+		 */
+		public function get( $key, $default = null ) {
+
+			if ( array_key_exists( $key, $this->handlers ) ) {
+				return $this->handlers[ $key ];
+			}
+
+			return $default;
+
+		}
+
+	}
+
+}
