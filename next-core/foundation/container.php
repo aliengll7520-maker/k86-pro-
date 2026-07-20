9c61882af4c7fb2057a1c966aa168539f2fedd3c
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-20 12:42:01 +0700

    Update container.php

--- next-core/foundation/container.php
+++ next-core/foundation/container.php
@@ -1,8 +1,67 @@
 <?php
 /**
- * Service Container
+ * K86 Pro Next Core
  *
- * Quản lý và cung cấp các dịch vụ của Foundation.
+ * Foundation Container
+ *
+ * Quản lý các service của Framework.
+ *
+ * @package K86Pro
  */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'K86_Container' ) ) {
+
+	class K86_Container {
+
+		/**
+		 * Danh sách service.
+		 *
+		 * @var array
+		 */
+		protected $services = array();
+
+		/**
+		 * Khởi tạo Container.
+		 *
+		 * @return void
+		 */
+		public function init() {
+
+			$this->services = array();
+
+		}
+
+		/**
+		 * Đăng ký service.
+		 *
+		 * @param string $key
+		 * @param mixed  $service
+		 * @return void
+		 */
+		public function set( $key, $service ) {
+
+			$this->services[ $key ] = $service;
+
+		}
+
+		/**
+		 * Lấy service.
+		 *
+		 * @param string $key
+		 * @param mixed  $default
+		 * @return mixed
+		 */
+		public function get( $key, $default = null ) {
+
+			if ( array_key_exists( $key, $this->services ) ) {
+				return $this->services[ $key ];
+			}
+
+			return $default;
+
+		}
+
+	}
+}
