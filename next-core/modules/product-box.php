1ce90c2469efcdfd6f6464a28e4ad2fc016c07c0
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-20 20:58:29 +0700

    Update product-box.php

--- next-core/modules/product-box.php
+++ next-core/modules/product-box.php
@@ -1,8 +1,51 @@
 <?php
 /**
+ * K86 Pro Next Core
  * Product Box Module
  *
- * Hiển thị hộp thông tin sản phẩm.
+ * @package K86Pro
  */
 
 defined('ABSPATH') || exit;
+
+if (!class_exists('K86_Product_Box_Module')) {
+
+    class K86_Product_Box_Module {
+
+        /**
+         * Product Service.
+         *
+         * @var K86_Product_Service
+         */
+        protected $service;
+
+        /**
+         * Product Renderer.
+         *
+         * @var K86_Product_Renderer
+         */
+        protected $renderer;
+
+        /**
+         * Constructor.
+         *
+         * @param K86_Product_Service  $service
+         * @param K86_Product_Renderer $renderer
+         */
+        public function __construct($service, $renderer) {
+            $this->service  = $service;
+            $this->renderer = $renderer;
+        }
+
+        /**
+         * Render Product Box.
+         *
+         * @return string
+         */
+        public function render() {
+            return $this->renderer->render();
+        }
+
+    }
+
+}
