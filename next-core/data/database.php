9c715b392013ea0ea57a6a1e9473c86ceccae4c5
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-20 17:54:27 +0700

    Update database.php

--- next-core/data/database.php
+++ next-core/data/database.php
@@ -1,1 +1,57 @@
 <?php
+/**
+ * K86 Pro Next Core
+ * Database Manager
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class K86_Database {
+
+	/**
+	 * WordPress Database
+	 *
+	 * @var wpdb
+	 */
+	protected $db;
+
+	/**
+	 * Tên bảng
+	 *
+	 * @var string
+	 */
+	protected $table;
+
+	/**
+	 * Khởi tạo
+	 */
+	public function __construct() {
+
+		global $wpdb;
+
+		$this->db = $wpdb;
+
+		$this->table = $wpdb->prefix . 'k86_products';
+	}
+
+	/**
+	 * Lấy đối tượng wpdb
+	 *
+	 * @return wpdb
+	 */
+	public function db() {
+		return $this->db;
+	}
+
+	/**
+	 * Lấy tên bảng
+	 *
+	 * @return string
+	 */
+	public function table() {
+		return $this->table;
+	}
+
+}
