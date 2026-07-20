f5846808a44837fe3668ceef21e2747e03e8b2e0
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-20 22:39:38 +0700

    Update service-provider.php

--- next-core/providers/service-provider.php
+++ next-core/providers/service-provider.php
@@ -13,68 +13,51 @@ if (!class_exists('K86_Service_Provider')) {
     class K86_Service_Provider {
 
         /**
-         * Register all framework services.
+         * Register framework services.
          *
          * @param K86_Container      $container
          * @param K86_Engine_Manager $manager
          */
         public function register($container, $manager) {
 
-            // Product Engine
-            $container->singleton('product', function () {
-                return new K86_Product_Engine();
-            });
+            /*
+             * Engines
+             */
+            $container->singleton('product', fn() => new K86_Product_Engine());
+            $container->singleton('media', fn() => new K86_Media_Engine());
+            $container->singleton('pricing', fn() => new K86_Pricing_Engine());
+            $container->singleton('review', fn() => new K86_Review_Engine());
+            $container->singleton('inventory', fn() => new K86_Inventory_Engine());
+            $container->singleton('voucher', fn() => new K86_Voucher_Engine());
+            $container->singleton('shipping', fn() => new K86_Shipping_Engine());
+            $container->singleton('warranty', fn() => new K86_Warranty_Engine());
+            $container->singleton('return_policy', fn() => new K86_Return_Policy_Engine());
 
-            // Media Engine
-            $container->singleton('media', function () {
-                return new K86_Media_Engine();
-            });
+            /*
+             * Core Services
+             */
+            $container->singleton('product_service', fn() => new K86_Product_Service($manager));
+            $container->singleton('health_check', fn() => new K86_Health_Check());
+            $container->singleton('module_registry', fn() => new K86_Module_Registry());
+            $container->singleton('wordpress_hooks', fn() => new K86_WordPress_Hooks());
+            $container->singleton('event_dispatcher', fn() => new K86_Event_Dispatcher());
 
-            // Pricing Engine
-            $container->singleton('pricing', function () {
-                return new K86_Pricing_Engine();
-            });
-
-            // Review Engine
-            $container->singleton('review', function () {
-                return new K86_Review_Engine();
-            });
-
-            // Inventory Engine
-            $container->singleton('inventory', function () {
-                return new K86_Inventory_Engine();
-            });
-
-            // Voucher Engine
-            $container->singleton('voucher', function () {
-                return new K86_Voucher_Engine();
-            });
-
-            // Shipping Engine
-            $container->singleton('shipping', function () {
-                return new K86_Shipping_Engine();
-            });
-
-            // Warranty Engine
-            $container->singleton('warranty', function () {
-                return new K86_Warranty_Engine();
-            });
-
-            // Return Policy Engine
-            $container->singleton('return_policy', function () {
-                return new K86_Return_Policy_Engine();
-            });
-
-            // Register engines
-            $manager->register('product', $container->get('product'));
-            $manager->register('media', $container->get('media'));
-            $manager->register('pricing', $container->get('pricing'));
-            $manager->register('review', $container->get('review'));
-            $manager->register('inventory', $container->get('inventory'));
-            $manager->register('voucher', $container->get('voucher'));
-            $manager->register('shipping', $container->get('shipping'));
-            $manager->register('warranty', $container->get('warranty'));
-            $manager->register('return_policy', $container->get('return_policy'));
+            /*
+             * Register Engines
+             */
+            foreach ([
+                'product',
+                'media',
+                'pricing',
+                'review',
+                'inventory',
+                'voucher',
+                'shipping',
+                'warranty',
+                'return_policy'
+            ] as $engine) {
+                $manager->register($engine, $container->get($engine));
+            }
         }
 
     }
