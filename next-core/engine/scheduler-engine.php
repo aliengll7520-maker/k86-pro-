be5f5ba0af4e0509f68e57ecad8fd4f2da7ba8b5
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-20 14:07:53 +0700

    Create scheduler-engine.php

---
+++ next-core/engine/scheduler-engine.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * K86 Pro Next Core
+ *
+ * Scheduler Engine
+ *
+ * Engine quản lý lập lịch tác vụ.
+ *
+ * @package K86Pro
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'K86_Scheduler_Engine' ) ) {
+
+	class K86_Scheduler_Engine {
+
+		/**
+		 * Danh sách scheduler handlers.
+		 *
+		 * @var array
+		 */
+		protected $handlers = array();
+
+		/**
+		 * Khởi tạo Engine.
+		 *
+		 * @return void
+		 */
+		public function init() {
+
+			$this->handlers = array();
+
+		}
+
+		/**
+		 * Đăng ký handler.
+		 *
+		 * @param string $key
+		 * @param mixed  $handler
+		 * @return void
+		 */
+		public function register( $key, $handler ) {
+
+			$this->handlers[ $key ] = $handler;
+
+		}
+
+		/**
+		 * Lấy handler.
+		 *
+		 * @param string $key
+		 * @param mixed  $default
+		 * @return mixed
+		 */
+		public function get( $key, $default = null ) {
+
+			if ( array_key_exists( $key, $this->handlers ) ) {
+				return $this->handlers[ $key ];
+			}
+
+			return $default;
+
+		}
+
+	}
+
+}
