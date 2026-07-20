ed68df7da44e2aed88a20fc56ad17fc82bb443ab
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-20 12:39:30 +0700

    Update registry.php

--- next-core/foundation/registry.php
+++ next-core/foundation/registry.php
@@ -1,8 +1,68 @@
 <?php
 /**
- * Registry
+ * K86 Pro Next Core
  *
- * Lưu trữ và quản lý các đối tượng dùng chung.
+ * Foundation Registry
+ *
+ * Quản lý đăng ký và truy xuất đối tượng của Framework.
+ *
+ * @package K86Pro
  */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'K86_Registry' ) ) {
+
+	class K86_Registry {
+
+		/**
+		 * Danh sách đối tượng đã đăng ký.
+		 *
+		 * @var array
+		 */
+		protected $items = array();
+
+		/**
+		 * Khởi tạo Registry.
+		 *
+		 * @return void
+		 */
+		public function init() {
+
+			$this->items = array();
+
+		}
+
+		/**
+		 * Đăng ký đối tượng.
+		 *
+		 * @param string $key
+		 * @param mixed  $object
+		 * @return void
+		 */
+		public function set( $key, $object ) {
+
+			$this->items[ $key ] = $object;
+
+		}
+
+		/**
+		 * Lấy đối tượng.
+		 *
+		 * @param string $key
+		 * @param mixed  $default
+		 * @return mixed
+		 */
+		public function get( $key, $default = null ) {
+
+			if ( array_key_exists( $key, $this->items ) ) {
+				return $this->items[ $key ];
+			}
+
+			return $default;
+
+		}
+
+	}
+
+}
