2414430bafac750e710983f5f1aa65aded555f71
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-20 13:08:05 +0700

    Update bootstrap.php

--- next-core/foundation/bootstrap.php
+++ next-core/foundation/bootstrap.php
@@ -1,8 +1,63 @@
 <?php
 /**
- * Bootstrap
+ * K86 Pro Next Core
  *
- * Khởi tạo Foundation của K86 Pro Next Core.
+ * Foundation Bootstrap
+ *
+ * Khởi tạo toàn bộ Foundation của Framework.
+ *
+ * @package K86Pro
  */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'K86_Bootstrap' ) ) {
+
+	class K86_Bootstrap {
+
+		/**
+		 * Loader.
+		 *
+		 * @var K86_Loader|null
+		 */
+		protected $loader = null;
+
+		/**
+		 * Khởi tạo Bootstrap.
+		 *
+		 * @return void
+		 */
+		public function init() {
+
+			$this->loader = null;
+
+		}
+
+		/**
+		 * Thiết lập Loader.
+		 *
+		 * @param K86_Loader $loader Đối tượng Loader.
+		 * @return void
+		 */
+		public function set_loader( $loader ) {
+
+			$this->loader = $loader;
+
+		}
+
+		/**
+		 * Khởi động Foundation.
+		 *
+		 * @return void
+		 */
+		public function boot() {
+
+			if ( $this->loader instanceof K86_Loader ) {
+				$this->loader->load();
+			}
+
+		}
+
+	}
+
+}
