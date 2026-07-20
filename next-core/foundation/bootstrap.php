ac97018f31eeec3e25d16cf345d33b239cac721a
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-20 19:45:38 +0700

    Update bootstrap.php

--- next-core/foundation/bootstrap.php
+++ next-core/foundation/bootstrap.php
@@ -13,101 +13,61 @@ if (!class_exists('K86_Bootstrap')) {
     class K86_Bootstrap {
 
         /**
-         * Registry.
-         *
-         * @var K86_Registry
-         */
-        protected $registry;
-
-        /**
-         * Container.
-         *
-         * @var K86_Container
-         */
-        protected $container;
-
-        /**
-         * Engine Manager.
-         *
-         * @var K86_Engine_Manager
-         */
-        protected $engine_manager;
-
-        /**
-         * Khởi động Next Core.
+         * Bootstrap Framework.
          */
         public function boot() {
 
-            // Nạp toàn bộ class
+            // Load framework.
             $loader = new K86_Loader();
             $loader->load();
 
-            // Registry
-            $this->registry = new K86_Registry();
+            // Core services.
+            $registry = new K86_Registry();
+            $container = new K86_Container();
+            $manager = new K86_Engine_Manager();
 
-            // Container
-            $this->container = new K86_Container();
+            /*
+             * Register services.
+             */
+            $container->singleton('product', function () {
+                return new K86_Product_Engine();
+            });
 
-            // Engine Manager
-            $this->engine_manager = new K86_Engine_Manager();
+            $container->singleton('media', function () {
+                return new K86_Media_Engine();
+            });
 
-            // Đăng ký Engine
-            $this->engine_manager->register(
-                'product',
-                new K86_Product_Engine()
-            );
+            $container->singleton('pricing', function () {
+                return new K86_Pricing_Engine();
+            });
 
-            $this->engine_manager->register(
-                'media',
-                new K86_Media_Engine()
-            );
+            $container->singleton('review', function () {
+                return new K86_Review_Engine();
+            });
 
-            $this->engine_manager->register(
-                'pricing',
-                new K86_Pricing_Engine()
-            );
+            $container->singleton('inventory', function () {
+                return new K86_Inventory_Engine();
+            });
 
-            $this->engine_manager->register(
-                'review',
-                new K86_Review_Engine()
-            );
+            $container->singleton('voucher', function () {
+                return new K86_Voucher_Engine();
+            });
 
-            $this->engine_manager->register(
-                'inventory',
-                new K86_Inventory_Engine()
-            );
+            /*
+             * Register engines.
+             */
+            $manager->register('product', $container->get('product'));
+            $manager->register('media', $container->get('media'));
+            $manager->register('pricing', $container->get('pricing'));
+            $manager->register('review', $container->get('review'));
+            $manager->register('inventory', $container->get('inventory'));
+            $manager->register('voucher', $container->get('voucher'));
 
-            $this->engine_manager->register(
-                'voucher',
-                new K86_Voucher_Engine()
-            );
-
-            // Đưa vào Registry
-            $this->registry->set('container', $this->container);
-            $this->registry->set('engine_manager', $this->engine_manager);
-
-            return $this;
-        }
-
-        /**
-         * Lấy Registry.
-         */
-        public function registry() {
-            return $this->registry;
-        }
-
-        /**
-         * Lấy Container.
-         */
-        public function container() {
-            return $this->container;
-        }
-
-        /**
-         * Lấy Engine Manager.
-         */
-        public function engine_manager() {
-            return $this->engine_manager;
+            /*
+             * Store services.
+             */
+            $registry->set('container', $container);
+            $registry->set('engine_manager', $manager);
         }
     }
 
