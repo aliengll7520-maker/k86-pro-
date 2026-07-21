6f1a602ff6f1c5a7f0ab3b045bd628704ddb6bd1
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-21 08:46:36 +0700

    Update pages.php

--- next-core/admin/pages.php
+++ next-core/admin/pages.php
@@ -1,1 +1,64 @@
 <?php
+/**
+ * K86 Pro Next Core
+ * Admin Pages
+ *
+ * @package K86Pro
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'K86_Admin_Pages' ) ) {
+
+	class K86_Admin_Pages {
+
+		/**
+		 * Dashboard.
+		 */
+		public static function dashboard() {
+			?>
+			<div class="wrap">
+				<h1>K86 Pro Dashboard</h1>
+				<p>Welcome to K86 Pro Next Core.</p>
+			</div>
+			<?php
+		}
+
+		/**
+		 * Products.
+		 */
+		public static function products() {
+			?>
+			<div class="wrap">
+				<h1>Product Manager</h1>
+				<p>Product list will be available in the next milestone.</p>
+			</div>
+			<?php
+		}
+
+		/**
+		 * Add Product.
+		 */
+		public static function add_product() {
+			?>
+			<div class="wrap">
+				<h1>Add Product</h1>
+				<p>Product form will be available in the next milestone.</p>
+			</div>
+			<?php
+		}
+
+		/**
+		 * Settings.
+		 */
+		public static function settings() {
+			?>
+			<div class="wrap">
+				<h1>Settings</h1>
+				<p>K86 Pro settings page.</p>
+			</div>
+			<?php
+		}
+	}
+
+}
