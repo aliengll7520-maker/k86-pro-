50997345d58827813d629099913d95cdcb33f876
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-21 18:56:22 +0700

    Update wordpress-hooks.php

--- next-core/compatibility/wordpress-hooks.php
+++ next-core/compatibility/wordpress-hooks.php
@@ -1,62 +1,86 @@
 <?php
 /**
+ * K86 Pro Next Core
  * WordPress Hooks
  *
- * Cầu nối giữa WordPress và Next Core.
+ * Bridge between WordPress lifecycle and Next Core.
  *
  * @package K86Pro
  */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
-if (!class_exists('K86_WordPress_Hooks')) {
+if ( ! class_exists( 'K86_WordPress_Hooks' ) ) {
 
-    class K86_WordPress_Hooks {
+	class K86_WordPress_Hooks {
 
-        /**
-         * Khởi tạo.
-         */
-        public function __construct() {
+		/**
+		 * Constructor.
+		 */
+		public function __construct() {
 
-            add_action('init', array($this, 'init'));
+			$this->register_hooks();
 
-            add_action('admin_init', array($this, 'admin_init'));
+		}
 
-            add_action('wp_loaded', array($this, 'wp_loaded'));
+		/**
+		 * Register WordPress hooks.
+		 *
+		 * @return void
+		 */
+		protected function register_hooks() {
 
-            add_action('plugins_loaded', array($this, 'plugins_loaded'));
-        }
+			add_action( 'plugins_loaded', array( $this, 'plugins_loaded' ) );
+			add_action( 'init', array( $this, 'init' ) );
+			add_action( 'admin_init', array( $this, 'admin_init' ) );
+			add_action( 'wp_loaded', array( $this, 'wp_loaded' ) );
 
-        /**
-         * WordPress init.
-         */
-        public function init() {
+		}
 
-            do_action('k86/core/init');
-        }
+		/**
+		 * WordPress plugins_loaded.
+		 *
+		 * @return void
+		 */
+		public function plugins_loaded() {
 
-        /**
-         * WordPress admin init.
-         */
-        public function admin_init() {
+			do_action( 'k86/core/plugins_loaded' );
 
-            do_action('k86/core/admin_init');
-        }
+		}
 
-        /**
-         * WordPress loaded.
-         */
-        public function wp_loaded() {
+		/**
+		 * WordPress init.
+		 *
+		 * @return void
+		 */
+		public function init() {
 
-            do_action('k86/core/wp_loaded');
-        }
+			do_action( 'k86/core/init' );
 
-        /**
-         * Plugins loaded.
-         */
-        public function plugins_loaded() {
+		}
 
-            do_action('k86/core/plugins_loaded');
-        }
-    }
+		/**
+		 * WordPress admin_init.
+		 *
+		 * @return void
+		 */
+		public function admin_init() {
+
+			do_action( 'k86/core/admin_init' );
+
+		}
+
+		/**
+		 * WordPress wp_loaded.
+		 *
+		 * @return void
+		 */
+		public function wp_loaded() {
+
+			do_action( 'k86/core/wp_loaded' );
+
+		}
+
+	}
+
 }
