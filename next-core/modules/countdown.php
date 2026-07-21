425d74c0531bb862ab8032fea91f8f8b53982bdb
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-21 17:06:43 +0700

    Update countdown.php

--- next-core/modules/countdown.php
+++ next-core/modules/countdown.php
@@ -4,8 +4,6 @@
  *
  * Countdown Module
  *
- * Module hiển thị đồng hồ đếm ngược chương trình khuyến mãi.
- *
  * @package K86Pro
  */
 
@@ -16,46 +14,62 @@ if ( ! class_exists( 'K86_Countdown_Module' ) ) {
 	class K86_Countdown_Module {
 
 		/**
-		 * Dữ liệu module.
+		 * Thứ tự hiển thị.
 		 *
-		 * @var array
+		 * @return int
 		 */
-		protected $data = array();
+		public function priority() {
 
-		/**
-		 * Khởi tạo module.
-		 *
-		 * @param array $data Dữ liệu sản phẩm.
-		 */
-		public function __construct( $data = array() ) {
-
-			$this->data = $data;
+			return 90;
 
 		}
 
 		/**
 		 * Render module.
 		 *
+		 * @param array $product Dữ liệu sản phẩm.
+		 *
 		 * @return string
 		 */
-		public function render() {
+		public function render( array $product = array() ) {
 
-			$end_time = '';
+			$countdown = array();
 
-			if ( isset( $this->data['countdown_end'] ) ) {
-				$end_time = sanitize_text_field( $this->data['countdown_end'] );
+			if (
+				isset( $product['countdown'] ) &&
+				is_array( $product['countdown'] )
+			) {
+				$countdown = $product['countdown'];
 			}
+
+			$enabled = ! empty( $countdown['enabled'] );
+			$title   = $countdown['title'] ?? '';
+			$end_time = $countdown['end_time'] ?? '';
 
 			ob_start();
 			?>
 
-			<div class="k86-countdown">
+			<div class="k86-product-countdown">
 
-				<?php if ( ! empty( $end_time ) ) : ?>
+				<?php if ( $enabled && ! empty( $end_time ) ) : ?>
 
-					<div class="k86-countdown-timer"
-						data-end="<?php echo esc_attr( $end_time ); ?>">
+					<?php if ( ! empty( $title ) ) : ?>
+						<div class="k86-countdown-title">
+							<?php echo esc_html( $title ); ?>
+						</div>
+					<?php endif; ?>
+
+					<div
+						class="k86-countdown-timer"
+						data-end-time="<?php echo esc_attr( $end_time ); ?>"
+					>
 						<?php echo esc_html( $end_time ); ?>
+					</div>
+
+				<?php else : ?>
+
+					<div class="k86-countdown-placeholder">
+						No active countdown.
 					</div>
 
 				<?php endif; ?>
