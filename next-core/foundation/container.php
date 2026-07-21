4d22896818b64a5efba52457fd5cabf69a6dce0e
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-21 19:06:47 +0700

    Update container.php

--- next-core/foundation/container.php
+++ next-core/foundation/container.php
@@ -6,124 +6,164 @@
  * @package K86Pro
  */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
-if (!class_exists('K86_Container')) {
+if ( ! class_exists( 'K86_Container' ) ) {
 
-    class K86_Container {
+	class K86_Container {
 
-        /**
-         * Registered services.
-         *
-         * @var array
-         */
-        protected $bindings = array();
+		/**
+		 * Registered service bindings.
+		 *
+		 * @var array
+		 */
+		protected $bindings = array();
 
-        /**
-         * Shared instances.
-         *
-         * @var array
-         */
-        protected $instances = array();
+		/**
+		 * Shared service instances.
+		 *
+		 * @var array
+		 */
+		protected $instances = array();
 
-        /**
-         * Register a factory.
-         *
-         * @param string   $key
-         * @param callable $factory
-         */
-        public function bind($key, callable $factory) {
-            $this->bindings[$key] = $factory;
-        }
+		/**
+		 * Register a factory binding.
+		 *
+		 * @param string   $key     Service key.
+		 * @param callable $factory Factory callback.
+		 *
+		 * @return void
+		 */
+		public function bind( $key, callable $factory ) {
 
-        /**
-         * Register a singleton.
-         *
-         * @param string   $key
-         * @param callable $factory
-         */
-        public function singleton($key, callable $factory) {
-            $this->bindings[$key] = function ($container) use ($key, $factory) {
+			$this->bindings[ $key ] = $factory;
 
-                if (!isset($this->instances[$key])) {
-                    $this->instances[$key] = $factory($container);
-                }
+		}
 
-                return $this->instances[$key];
-            };
-        }
+		/**
+		 * Register a singleton binding.
+		 *
+		 * @param string   $key     Service key.
+		 * @param callable $factory Factory callback.
+		 *
+		 * @return void
+		 */
+		public function singleton( $key, callable $factory ) {
 
-        /**
-         * Register an existing instance.
-         *
-         * @param string $key
-         * @param mixed  $instance
-         */
-        public function instance($key, $instance) {
-            $this->instances[$key] = $instance;
-        }
+			$this->bindings[ $key ] = function ( $container ) use ( $key, $factory ) {
 
-        /**
-         * Resolve a service.
-         *
-         * @param string $key
-         *
-         * @return mixed|null
-         */
-        public function get($key) {
+				if ( ! isset( $this->instances[ $key ] ) ) {
+					$this->instances[ $key ] = $factory( $container );
+				}
 
-            if (isset($this->instances[$key])) {
-                return $this->instances[$key];
-            }
+				return $this->instances[ $key ];
 
-            if (!isset($this->bindings[$key])) {
-                return null;
-            }
+			};
 
-            return call_user_func($this->bindings[$key], $this);
-        }
+		}
 
-        /**
-         * Check if a service exists.
-         *
-         * @param string $key
-         *
-         * @return bool
-         */
-        public function has($key) {
-            return isset($this->bindings[$key]) || isset($this->instances[$key]);
-        }
+		/**
+		 * Register an existing instance.
+		 *
+		 * @param string $key      Service key.
+		 * @param mixed  $instance Service instance.
+		 *
+		 * @return void
+		 */
+		public function instance( $key, $instance ) {
 
-        /**
-         * Remove a service.
-         *
-         * @param string $key
-         */
-        public function remove($key) {
-            unset($this->bindings[$key], $this->instances[$key]);
-        }
+			$this->instances[ $key ] = $instance;
 
-        /**
-         * Remove all services.
-         */
-        public function clear() {
-            $this->bindings = array();
-            $this->instances = array();
-        }
+		}
 
-        /**
-         * Get all registered keys.
-         *
-         * @return array
-         */
-        public function keys() {
-            return array_unique(
-                array_merge(
-                    array_keys($this->bindings),
-                    array_keys($this->instances)
-                )
-            );
-        }
-    }
+		/**
+		 * Resolve a service.
+		 *
+		 * @param string $key Service key.
+		 *
+		 * @return mixed|null
+		 */
+		public function get( $key ) {
+
+			if ( isset( $this->instances[ $key ] ) ) {
+				return $this->instances[ $key ];
+			}
+
+			if ( ! isset( $this->bindings[ $key ] ) ) {
+				return null;
+			}
+
+			return call_user_func(
+				$this->bindings[ $key ],
+				$this
+			);
+
+		}
+
+		/**
+		 * Check whether a service exists.
+		 *
+		 * @param string $key Service key.
+		 *
+		 * @return bool
+		 */
+		public function has( $key ) {
+
+			return isset( $this->bindings[ $key ] )
+				|| isset( $this->instances[ $key ] );
+
+		}
+
+		/**
+		 * Remove a service.
+		 *
+		 * @param string $key Service key.
+		 *
+		 * @return bool
+		 */
+		public function remove( $key ) {
+
+			$exists = $this->has( $key );
+
+			unset(
+				$this->bindings[ $key ],
+				$this->instances[ $key ]
+			);
+
+			return $exists;
+
+		}
+
+		/**
+		 * Remove all services.
+		 *
+		 * @return void
+		 */
+		public function clear() {
+
+			$this->bindings  = array();
+			$this->instances = array();
+
+		}
+
+		/**
+		 * Get all registered service keys.
+		 *
+		 * @return array
+		 */
+		public function keys() {
+
+			return array_values(
+				array_unique(
+					array_merge(
+						array_keys( $this->bindings ),
+						array_keys( $this->instances )
+					)
+				)
+			);
+
+		}
+
+	}
 
 }
