066f9e6e2b6d32edba4c23329f38ce36a83ad8f8
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-21 07:14:08 +0700

    Update shipping-engine.php

--- next-core/engine/shipping-engine.php
+++ next-core/engine/shipping-engine.php
@@ -6,84 +6,100 @@
  * @package K86Pro
  */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
-if (!class_exists('K86_Shipping_Engine')) {
+if ( ! class_exists( 'K86_Shipping_Engine' ) ) {
 
-    class K86_Shipping_Engine extends K86_Engine_Base {
+	class K86_Shipping_Engine extends K86_Engine_Base {
 
-        /**
-         * Set shipping method.
-         *
-         * @param string $method
-         */
-        public function set_method($method) {
-            $this->set('shipping_method', sanitize_text_field($method));
-        }
+		/**
+		 * Thiết lập phương thức vận chuyển.
+		 *
+		 * @param string $method
+		 * @return $this
+		 */
+		public function set_method( $method ) {
+			return $this->register( 'shipping_method', sanitize_text_field( $method ) );
+		}
 
-        /**
-         * Get shipping method.
-         *
-         * @return string
-         */
-        public function method() {
-            return $this->get('shipping_method', '');
-        }
+		/**
+		 * Lấy phương thức vận chuyển.
+		 *
+		 * @return string
+		 */
+		public function get_method() {
+			return $this->get( 'shipping_method', '' );
+		}
 
-        /**
-         * Set shipping fee.
-         *
-         * @param float $fee
-         */
-        public function set_fee($fee) {
-            $this->set('shipping_fee', (float) $fee);
-        }
+		/**
+		 * Thiết lập phí vận chuyển.
+		 *
+		 * @param float $fee
+		 * @return $this
+		 */
+		public function set_fee( $fee ) {
+			return $this->register( 'shipping_fee', (float) $fee );
+		}
 
-        /**
-         * Get shipping fee.
-         *
-         * @return float
-         */
-        public function fee() {
-            return (float) $this->get('shipping_fee', 0);
-        }
+		/**
+		 * Lấy phí vận chuyển.
+		 *
+		 * @return float
+		 */
+		public function get_fee() {
+			return (float) $this->get( 'shipping_fee', 0 );
+		}
 
-        /**
-         * Set free shipping.
-         *
-         * @param bool $enabled
-         */
-        public function set_free_shipping($enabled) {
-            $this->set('free_shipping', (bool) $enabled);
-        }
+		/**
+		 * Thiết lập miễn phí vận chuyển.
+		 *
+		 * @param bool $enabled
+		 * @return $this
+		 */
+		public function set_free_shipping( $enabled ) {
+			return $this->register( 'free_shipping', (bool) $enabled );
+		}
 
-        /**
-         * Check free shipping.
-         *
-         * @return bool
-         */
-        public function is_free_shipping() {
-            return (bool) $this->get('free_shipping', false);
-        }
+		/**
+		 * Kiểm tra miễn phí vận chuyển.
+		 *
+		 * @return bool
+		 */
+		public function is_free_shipping() {
+			return (bool) $this->get( 'free_shipping', false );
+		}
 
-        /**
-         * Set estimated delivery.
-         *
-         * @param string $estimate
-         */
-        public function set_estimate($estimate) {
-            $this->set('delivery_estimate', sanitize_text_field($estimate));
-        }
+		/**
+		 * Thiết lập thời gian giao hàng dự kiến.
+		 *
+		 * @param string $estimate
+		 * @return $this
+		 */
+		public function set_estimate( $estimate ) {
+			return $this->register( 'delivery_estimate', sanitize_text_field( $estimate ) );
+		}
 
-        /**
-         * Get estimated delivery.
-         *
-         * @return string
-         */
-        public function estimate() {
-            return $this->get('delivery_estimate', '');
-        }
+		/**
+		 * Lấy thời gian giao hàng dự kiến.
+		 *
+		 * @return string
+		 */
+		public function get_estimate() {
+			return $this->get( 'delivery_estimate', '' );
+		}
 
-    }
+		/**
+		 * Tính phí vận chuyển thực tế.
+		 *
+		 * @return float
+		 */
+		public function calculate_shipping_fee() {
 
+			if ( $this->is_free_shipping() ) {
+				return 0;
+			}
+
+			return $this->get_fee();
+		}
+	}
 }
