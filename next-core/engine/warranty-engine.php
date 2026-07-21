c4c630c880e60369a5ad09809e33926e90bdc123
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-21 07:16:33 +0700

    Update warranty-engine.php

--- next-core/engine/warranty-engine.php
+++ next-core/engine/warranty-engine.php
@@ -6,75 +6,90 @@
  * @package K86Pro
  */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
-if (!class_exists('K86_Warranty_Engine')) {
+if ( ! class_exists( 'K86_Warranty_Engine' ) ) {
 
-    class K86_Warranty_Engine extends K86_Engine_Base {
+	class K86_Warranty_Engine extends K86_Engine_Base {
 
-        /**
-         * Set warranty period.
-         *
-         * @param string $period
-         */
-        public function set_period($period) {
-            $this->set('warranty_period', sanitize_text_field($period));
-        }
+		/**
+		 * Thiết lập thời gian bảo hành.
+		 *
+		 * @param string $period
+		 * @return $this
+		 */
+		public function set_period( $period ) {
+			return $this->register( 'warranty_period', sanitize_text_field( $period ) );
+		}
 
-        /**
-         * Get warranty period.
-         *
-         * @return string
-         */
-        public function period() {
-            return $this->get('warranty_period', '');
-        }
+		/**
+		 * Lấy thời gian bảo hành.
+		 *
+		 * @return string
+		 */
+		public function get_period() {
+			return $this->get( 'warranty_period', '' );
+		}
 
-        /**
-         * Set warranty type.
-         *
-         * @param string $type
-         */
-        public function set_type($type) {
-            $this->set('warranty_type', sanitize_text_field($type));
-        }
+		/**
+		 * Thiết lập loại bảo hành.
+		 *
+		 * @param string $type
+		 * @return $this
+		 */
+		public function set_type( $type ) {
+			return $this->register( 'warranty_type', sanitize_text_field( $type ) );
+		}
 
-        /**
-         * Get warranty type.
-         *
-         * @return string
-         */
-        public function type() {
-            return $this->get('warranty_type', '');
-        }
+		/**
+		 * Lấy loại bảo hành.
+		 *
+		 * @return string
+		 */
+		public function get_type() {
+			return $this->get( 'warranty_type', '' );
+		}
 
-        /**
-         * Set warranty provider.
-         *
-         * @param string $provider
-         */
-        public function set_provider($provider) {
-            $this->set('warranty_provider', sanitize_text_field($provider));
-        }
+		/**
+		 * Thiết lập đơn vị bảo hành.
+		 *
+		 * @param string $provider
+		 * @return $this
+		 */
+		public function set_provider( $provider ) {
+			return $this->register( 'warranty_provider', sanitize_text_field( $provider ) );
+		}
 
-        /**
-         * Get warranty provider.
-         *
-         * @return string
-         */
-        public function provider() {
-            return $this->get('warranty_provider', '');
-        }
+		/**
+		 * Lấy đơn vị bảo hành.
+		 *
+		 * @return string
+		 */
+		public function get_provider() {
+			return $this->get( 'warranty_provider', '' );
+		}
 
-        /**
-         * Check if warranty exists.
-         *
-         * @return bool
-         */
-        public function has_warranty() {
-            return $this->period() !== '';
-        }
+		/**
+		 * Kiểm tra sản phẩm có bảo hành.
+		 *
+		 * @return bool
+		 */
+		public function has_warranty() {
+			return '' !== $this->get_period();
+		}
 
-    }
+		/**
+		 * Lấy thông tin bảo hành.
+		 *
+		 * @return array
+		 */
+		public function get_warranty_info() {
+			return array(
+				'period'   => $this->get_period(),
+				'type'     => $this->get_type(),
+				'provider' => $this->get_provider(),
+			);
+		}
+	}
 
 }
