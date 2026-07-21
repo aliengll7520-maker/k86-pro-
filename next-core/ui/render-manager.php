44dff977310933e90d05ec6fa8988c0453ad39f5
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-22 06:11:34 +0700

    Update render-manager.php

--- next-core/ui/render-manager.php
+++ next-core/ui/render-manager.php
@@ -23,6 +23,29 @@ if ( ! class_exists( 'K86_Render_Manager' ) ) {
 		protected $data = array();
 
 		/**
+		 * Render Context.
+		 *
+		 * @var K86_Render_Context|null
+		 */
+		protected $context = null;
+
+		/**
+		 * HTML Builder.
+		 *
+		 * @var K86_HTML_Builder|null
+		 */
+		protected $builder = null;
+
+		/**
+		 * Khởi tạo.
+		 */
+		public function __construct() {
+
+			$this->init();
+
+		}
+
+		/**
 		 * Khởi tạo.
 		 *
 		 * @return void
@@ -31,15 +54,26 @@ if ( ! class_exists( 'K86_Render_Manager' ) ) {
 
 			$this->data = array();
 
+			if ( class_exists( 'K86_Render_Context' ) ) {
+				$this->context = new K86_Render_Context();
+			}
+
+			if ( class_exists( 'K86_HTML_Builder' ) ) {
+				$this->builder = new K86_HTML_Builder(
+					$this->context
+				);
+			}
+
 		}
 
 		/**
 		 * Thiết lập dữ liệu.
 		 *
 		 * @param array $data Dữ liệu render.
+		 *
 		 * @return void
 		 */
-		public function set_data( $data ) {
+		public function set_data( array $data ) {
 
 			$this->data = $data;
 
@@ -57,15 +91,58 @@ if ( ! class_exists( 'K86_Render_Manager' ) ) {
 		}
 
 		/**
+		 * Lấy Render Context.
+		 *
+		 * @return K86_Render_Context|null
+		 */
+		public function get_context() {
+
+			return $this->context;
+
+		}
+
+		/**
+		 * Lấy HTML Builder.
+		 *
+		 * @return K86_HTML_Builder|null
+		 */
+		public function get_builder() {
+
+			return $this->builder;
+
+		}
+
+		/**
 		 * Render giao diện.
 		 *
 		 * @return string
 		 */
 		public function render() {
 
+			if ( ! $this->context || ! $this->builder ) {
+				$this->init();
+			}
+
+			if ( ! $this->context || ! $this->builder ) {
+				return '';
+			}
+
+			if ( method_exists( $this->context, 'replace' ) ) {
+				$this->context->replace( $this->data );
+			}
+
+			if ( method_exists( $this->builder, 'build' ) ) {
+				return $this->builder->build();
+			}
+
+			if ( method_exists( $this->builder, 'render' ) ) {
+				return $this->builder->render();
+			}
+
 			return '';
 
 		}
 
 	}
+
 }
