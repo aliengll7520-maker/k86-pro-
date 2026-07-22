4a7b59849f697160d6975dbb5615bebe8afbabae
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-22 12:15:10 +0700

    Update fields.php

--- next-core/admin/products/fields.php
+++ next-core/admin/products/fields.php
@@ -1,33 +1,133 @@
 <?php
+/**
+ * K86 Pro Next Core
+ *
+ * Product Field Registry
+ *
+ * @package K86Pro
+ */
 
 defined( 'ABSPATH' ) || exit;
 
 return array(
 
-    'basic' => array(),
+	'basic' => array(
 
-    'media' => array(),
+		'title' => array(
+			'type'     => 'text',
+			'sanitize' => 'sanitize_text_field',
+		),
 
-    'pricing' => array(),
+		'slug' => array(
+			'type'     => 'text',
+			'sanitize' => 'sanitize_title',
+		),
 
-    'actions' => array(),
+		'sku' => array(
+			'type'     => 'text',
+			'sanitize' => 'sanitize_text_field',
+		),
 
-    'rating' => array(),
+		'brand' => array(
+			'type'     => 'text',
+			'sanitize' => 'sanitize_text_field',
+		),
 
-    'voucher' => array(),
+		'status' => array(
+			'type'     => 'text',
+			'sanitize' => 'sanitize_text_field',
+		),
 
-    'description' => array(),
+	),
 
-    'compare' => array(),
+	'media' => array(
 
-    'countdown' => array(),
+		'image' => array(
+			'type'     => 'url',
+			'sanitize' => 'esc_url_raw',
+		),
 
-    'stock' => array(),
+		'gallery' => array(
+			'type'     => 'textarea',
+			'sanitize' => 'sanitize_textarea_field',
+		),
 
-    'shipping' => array(),
+		'video' => array(
+			'type'     => 'url',
+			'sanitize' => 'esc_url_raw',
+		),
 
-    'policy' => array(),
+	),
 
-    'faq' => array(),
+	'pricing' => array(
+
+		'price' => array(
+			'type'     => 'number',
+			'sanitize' => 'floatval',
+		),
+
+		'sale_price' => array(
+			'type'     => 'number',
+			'sanitize' => 'floatval',
+		),
+
+		'currency' => array(
+			'type'     => 'text',
+			'sanitize' => 'sanitize_text_field',
+		),
+
+		'discount' => array(
+			'type'     => 'number',
+			'sanitize' => 'floatval',
+		),
+
+		'saved' => array(
+			'type'     => 'number',
+			'sanitize' => 'floatval',
+		),
+
+	),
+
+	'actions' => array(
+
+		'buy_url' => array(
+			'type'     => 'url',
+			'sanitize' => 'esc_url_raw',
+		),
+
+		'shopee_url' => array(
+			'type'     => 'url',
+			'sanitize' => 'esc_url_raw',
+		),
+
+		'tiktok_url' => array(
+			'type'     => 'url',
+			'sanitize' => 'esc_url_raw',
+		),
+
+		'lazada_url' => array(
+			'type'     => 'url',
+			'sanitize' => 'esc_url_raw',
+		),
+
+	),
+
+	'rating' => array(),
+
+	'description' => array(),
+
+	'compare' => array(),
+
+	'voucher' => array(),
+
+	'countdown' => array(),
+
+	'stock' => array(),
+
+	'shipping' => array(),
+
+	'policy' => array(),
+
+	'faq' => array(),
 
 );
