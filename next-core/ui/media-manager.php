52fb6721ad966b53168ea520b3907180dfd8dcd5
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-22 08:38:53 +0700

    Update media-manager.php

--- next-core/ui/media-manager.php
+++ next-core/ui/media-manager.php
@@ -1,1 +1,22 @@
+<?php
+/**
+ * Media Manager.
+ *
+ * @package K86Pro
+ */
 
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'K86_Media_Manager' ) ) {
+
+	class K86_Media_Manager {
+
+		/**
+		 * Constructor.
+		 */
+		public function __construct() {
+		}
+
+	}
+
+}
