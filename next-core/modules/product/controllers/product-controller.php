0ba6035f76b90231c65e0788f94d942a93b9a3dc
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-22 23:15:17 +0700

    Update product-controller.php

--- next-core/modules/product/controllers/product-controller.php
+++ next-core/modules/product/controllers/product-controller.php
@@ -1,29 +1,41 @@
 <?php
 /**
+ * K86 Pro
  * Product Controller
+ *
+ * @package K86Pro
  */
 
 defined( 'ABSPATH' ) || exit;
 
 class K86_Product_Controller {
 
+	/**
+	 * Product Manager.
+	 *
+	 * @var K86_Product_Manager
+	 */
 	protected $manager;
 
+	/**
+	 * Constructor.
+	 */
 	public function __construct() {
 
 		$this->manager = new K86_Product_Manager();
 
 	}
 
-}
 	/**
 	 * Create product.
 	 *
 	 * @param array $product Product data.
-	 * @return bool
+	 * @return mixed
 	 */
 	public function create( array $product ) {
 
 		return $this->manager->create_product( $product );
 
-  }
+	}
+
+}
