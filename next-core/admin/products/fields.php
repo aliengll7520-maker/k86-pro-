49b1f4badfbd7a38053c14f20b500ebd97fd27e2
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-22 12:13:19 +0700

    Create fields.php

---
+++ next-core/admin/products/fields.php
@@ -0,0 +1,33 @@
+<?php
+
+defined( 'ABSPATH' ) || exit;
+
+return array(
+
+    'basic' => array(),
+
+    'media' => array(),
+
+    'pricing' => array(),
+
+    'actions' => array(),
+
+    'rating' => array(),
+
+    'voucher' => array(),
+
+    'description' => array(),
+
+    'compare' => array(),
+
+    'countdown' => array(),
+
+    'stock' => array(),
+
+    'shipping' => array(),
+
+    'policy' => array(),
+
+    'faq' => array(),
+
+);
