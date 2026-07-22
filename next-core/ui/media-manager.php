6dace2043534e9131f4d283a5f1bea4f6ca2a66f
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-22 09:46:20 +0700

    Update media-manager.php

--- next-core/ui/media-manager.php
+++ next-core/ui/media-manager.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * Media Manager.
+ * K86 Pro Next Core
+ *
+ * Media Manager
  *
  * @package K86Pro
  */
@@ -12,26 +14,47 @@ if ( ! class_exists( 'K86_Media_Manager' ) ) {
 	class K86_Media_Manager {
 
 		/**
-		 * Constructor.
-		 */
-		public function __construct() {
-		}
-
-		/**
-		 * Get product media.
+		 * Get all media for a product.
 		 *
 		 * @param array $product Product data.
 		 *
 		 * @return array
 		 */
-		public function get_product_media( $product ) {
+		public function get_product_media( array $product ) {
 
-			return array(
+			$media = array(
 				'featured'  => '',
 				'gallery'   => array(),
 				'video'     => '',
 				'downloads' => array(),
 			);
+
+			// Featured image.
+			if ( ! empty( $product['image'] ) ) {
+				$media['featured'] = $product['image'];
+			}
+
+			// Gallery.
+			if ( ! empty( $product['gallery'] ) && is_array( $product['gallery'] ) ) {
+				$media['gallery'] = $product['gallery'];
+			}
+
+			// Video.
+			if ( ! empty( $product['video'] ) ) {
+				$media['video'] = $product['video'];
+			}
+
+			// Downloads.
+			if ( ! empty( $product['downloads'] ) && is_array( $product['downloads'] ) ) {
+				$media['downloads'] = $product['downloads'];
+			}
+
+			// Fallback gallery.
+			if ( empty( $media['gallery'] ) && ! empty( $media['featured'] ) ) {
+				$media['gallery'][] = $media['featured'];
+			}
+
+			return $media;
 
 		}
 
