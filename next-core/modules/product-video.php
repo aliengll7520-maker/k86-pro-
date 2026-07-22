70a8355d7c6a435137219124468c9f1f51861c18
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-22 07:09:57 +0700

    Update product-video.php

--- next-core/modules/product-video.php
+++ next-core/modules/product-video.php
@@ -14,7 +14,7 @@ if ( ! class_exists( 'K86_Product_Video_Module' ) ) {
 	class K86_Product_Video_Module {
 
 		/**
-		 * Thứ tự hiển thị.
+		 * Module priority.
 		 *
 		 * @return int
 		 */
@@ -27,24 +27,21 @@ if ( ! class_exists( 'K86_Product_Video_Module' ) ) {
 		/**
 		 * Render module.
 		 *
-		 * @param array $product Dữ liệu sản phẩm.
+		 * @param array $product Product data.
 		 *
 		 * @return string
 		 */
-		public function render( array $product = array() ) {
+		public function render( array $product ) {
 
-			$video = '';
-
-			if ( ! empty( $product['video'] ) ) {
-				$video = esc_url( $product['video'] );
-			}
+			$video = ! empty( $product['video'] ) ? esc_url( $product['video'] ) : '';
+			$image = ! empty( $product['image'] ) ? esc_url( $product['image'] ) : '';
 
 			ob_start();
 			?>
 
 			<div class="k86-product-video">
 
-				<?php if ( ! empty( $video ) ) : ?>
+				<?php if ( $video ) : ?>
 
 					<video
 						class="k86-video-player"
@@ -52,14 +49,16 @@ if ( ! class_exists( 'K86_Product_Video_Module' ) ) {
 						preload="metadata"
 						playsinline
 					>
-						<source src="<?php echo esc_url( $video ); ?>" type="video/mp4">
+						<source src="<?php echo $video; ?>">
 					</video>
 
-				<?php else : ?>
+				<?php elseif ( $image ) : ?>
 
-					<div class="k86-video-placeholder">
-						No product video.
-					</div>
+					<img
+						class="k86-video-placeholder"
+						src="<?php echo $image; ?>"
+						alt=""
+					/>
 
 				<?php endif; ?>
 
