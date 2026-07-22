089b9f86dd629601aaf0d33abc4d42c95ce09f8a
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-22 16:22:56 +0700

    Update product-repository.php

--- next-core/repositories/product-repository.php
+++ next-core/repositories/product-repository.php
@@ -1,7 +1,76 @@
 <?php
+/**
+ * K86 Pro Next Core
+ * Product Repository
+ *
+ * @package K86Pro
+ */
 
 defined( 'ABSPATH' ) || exit;
 
-class K86_Product_Repository {
+if ( ! class_exists( 'K86_Product_Repository' ) ) {
 
+	class K86_Product_Repository {
+
+		/**
+		 * Save product.
+		 *
+		 * @param array $product Product data.
+		 * @return bool
+		 */
+		public function save( array $product ) {
+
+			return false;
+
+		}
+
+		/**
+		 * Update product.
+		 *
+		 * @param int   $id Product ID.
+		 * @param array $product Product data.
+		 * @return bool
+		 */
+		public function update( $id, array $product ) {
+
+			return false;
+
+		}
+
+		/**
+		 * Delete product.
+		 *
+		 * @param int $id Product ID.
+		 * @return bool
+		 */
+		public function delete( $id ) {
+
+			return false;
+
+		}
+
+		/**
+		 * Find one product.
+		 *
+		 * @param int $id Product ID.
+		 * @return array
+		 */
+		public function find( $id ) {
+
+			return array();
+
+		}
+
+		/**
+		 * Find all products.
+		 *
+		 * @return array
+		 */
+		public function find_all() {
+
+			return array();
+
+		}
+
+	}
 }
