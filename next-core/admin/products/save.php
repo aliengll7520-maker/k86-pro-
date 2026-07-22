ea639648d86acca007f745091ff4afb1c5401255
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-23 06:55:32 +0700

    Update save.php

--- next-core/admin/products/save.php
+++ next-core/admin/products/save.php
@@ -36,10 +36,25 @@ function k86_save_product() {
 
 	// Chuẩn bị dữ liệu.
 	$product = array(
+
+		// Thông tin cơ bản.
 		'title'      => isset( $_POST['k86_title'] ) ? sanitize_text_field( wp_unslash( $_POST['k86_title'] ) ) : '',
 		'price'      => isset( $_POST['k86_price'] ) ? floatval( wp_unslash( $_POST['k86_price'] ) ) : 0,
 		'sale_price' => isset( $_POST['k86_sale_price'] ) ? floatval( wp_unslash( $_POST['k86_sale_price'] ) ) : 0,
 		'status'     => isset( $_POST['k86_status'] ) ? sanitize_text_field( wp_unslash( $_POST['k86_status'] ) ) : 'draft',
+
+		// Media.
+		'image'      => isset( $_POST['k86_image'] ) ? esc_url_raw( wp_unslash( $_POST['k86_image'] ) ) : '',
+		'gallery'    => isset( $_POST['k86_gallery'] ) ? sanitize_textarea_field( wp_unslash( $_POST['k86_gallery'] ) ) : '',
+		'video'      => isset( $_POST['k86_video'] ) ? esc_url_raw( wp_unslash( $_POST['k86_video'] ) ) : '',
+		'youtube'    => isset( $_POST['k86_youtube'] ) ? esc_url_raw( wp_unslash( $_POST['k86_youtube'] ) ) : '',
+		'tiktok'     => isset( $_POST['k86_tiktok'] ) ? esc_url_raw( wp_unslash( $_POST['k86_tiktok'] ) ) : '',
+		'pdf'        => isset( $_POST['k86_pdf'] ) ? esc_url_raw( wp_unslash( $_POST['k86_pdf'] ) ) : '',
+		'documents'  => isset( $_POST['k86_documents'] ) ? sanitize_textarea_field( wp_unslash( $_POST['k86_documents'] ) ) : '',
+		'audio'      => isset( $_POST['k86_audio'] ) ? sanitize_textarea_field( wp_unslash( $_POST['k86_audio'] ) ) : '',
+		'icon'       => isset( $_POST['k86_icon'] ) ? esc_url_raw( wp_unslash( $_POST['k86_icon'] ) ) : '',
+		'downloads'  => isset( $_POST['k86_downloads'] ) ? sanitize_textarea_field( wp_unslash( $_POST['k86_downloads'] ) ) : '',
+
 	);
 
 	/**
