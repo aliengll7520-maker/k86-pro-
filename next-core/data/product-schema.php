35946fe8018c56ab16be609f04c0bbc0262ef5e8
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-22 16:42:21 +0700

    Create product-schema.php

---
+++ next-core/data/product-schema.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * K86 Pro Next Core
+ * Product Schema
+ *
+ * @package K86Pro
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( ! function_exists( 'k86_product_schema' ) ) {
+
+	/**
+	 * Return Product schema definition.
+	 *
+	 * @return array
+	 */
+	function k86_product_schema() {
+
+		global $wpdb;
+
+		return array(
+
+			'table' => $wpdb->prefix . 'k86_products',
+
+			'primary_key' => 'id',
+
+			'columns' => array(
+
+				'id'         => 'BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT',
+				'title'      => 'TEXT NOT NULL',
+				'price'      => 'DECIMAL(18,2) NOT NULL DEFAULT 0',
+				'sale_price' => 'DECIMAL(18,2) NOT NULL DEFAULT 0',
+				'status'     => 'VARCHAR(20) NOT NULL DEFAULT "draft"',
+
+				'created_at' => 'DATETIME NULL',
+				'updated_at' => 'DATETIME NULL',
+
+			),
+
+		);
+
+	}
+
+}
