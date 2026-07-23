499cafa6993dc30b6d8f01fdb6b62e05d8f93a97
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-23 08:36:49 +0700

    Update mapper.php

--- next-core/data/mapper.php
+++ next-core/data/mapper.php
@@ -13,7 +13,7 @@ if ( ! class_exists( 'K86_Product_Mapper' ) ) {
 	class K86_Product_Mapper {
 
 		/**
-		 * Chuyển dữ liệu DB thành Product Model.
+		 * Chuyển dữ liệu Database thành Product Model.
 		 *
 		 * @param array $row
 		 * @return K86_Product_Model|null
@@ -36,11 +36,38 @@ if ( ! class_exists( 'K86_Product_Mapper' ) ) {
 		 */
 		public static function to_array( K86_Product_Model $product ) {
 
+			if ( method_exists( $product, 'get_data' ) ) {
+				return $product->get_data();
+			}
+
 			if ( method_exists( $product, 'to_array' ) ) {
 				return $product->to_array();
 			}
 
 			return get_object_vars( $product );
+
+		}
+
+		/**
+		 * Kiểm tra Model hợp lệ.
+		 *
+		 * @param mixed $product
+		 * @return bool
+		 */
+		public static function is_valid( $product ) {
+
+			return $product instanceof K86_Product_Model;
+
+		}
+
+		/**
+		 * Tạo Model rỗng.
+		 *
+		 * @return K86_Product_Model
+		 */
+		public static function empty_model() {
+
+			return new K86_Product_Model();
 
 		}
 
