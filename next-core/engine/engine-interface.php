6e8d147c5bd5f8e028ccd446f6aec7188f7cac41
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-24 00:10:30 +0700

    Update engine-interface.php

--- next-core/engine/engine-interface.php
+++ next-core/engine/engine-interface.php
@@ -1,1 +1,60 @@
+<?php
+/**
+ * K86 Pro Next Core
+ * Engine Interface
+ *
+ * @package K86Pro
+ * @since   1.6.0
+ */
 
+defined( 'ABSPATH' ) || exit;
+
+if ( ! interface_exists( 'K86_Engine_Interface' ) ) {
+
+	interface K86_Engine_Interface {
+
+		/**
+		 * Initialize the engine.
+		 *
+		 * @return void
+		 */
+		public function init();
+
+		/**
+		 * Boot the engine.
+		 *
+		 * @return void
+		 */
+		public function boot();
+
+		/**
+		 * Shutdown the engine.
+		 *
+		 * @return void
+		 */
+		public function shutdown();
+
+		/**
+		 * Get the engine name.
+		 *
+		 * @return string
+		 */
+		public function get_name();
+
+		/**
+		 * Get the engine version.
+		 *
+		 * @return string
+		 */
+		public function get_version();
+
+		/**
+		 * Determine whether the engine is enabled.
+		 *
+		 * @return bool
+		 */
+		public function is_enabled();
+
+	}
+
+}
