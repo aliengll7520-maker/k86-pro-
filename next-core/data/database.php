e9ce878a7eb8524c2914137db78385fd2a7b4907
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-23 07:21:20 +0700

    Update database.php

--- next-core/data/database.php
+++ next-core/data/database.php
@@ -88,3 +88,111 @@ class K86_Database {
 	}
 
 }
+if ( ! class_exists( 'K86_Product_Database_Storage' ) ) {
+
+	class K86_Product_Database_Storage implements K86_Product_Storage_Interface {
+
+		/**
+		 * Database manager.
+		 *
+		 * @var K86_Database
+		 */
+		protected $database;
+
+		/**
+		 * Constructor.
+		 *
+		 * @param K86_Database|null $database Database manager.
+		 */
+		public function __construct( K86_Database $database = null ) {
+
+			$this->database = $database ? $database : new K86_Database();
+
+		}
+
+		/**
+		 * Find by ID.
+		 *
+		 * @param int $id Product ID.
+		 * @return K86_Product_Model|null
+		 */
+		public function find( $id ) {
+
+			return null;
+
+		}
+
+		/**
+		 * Find by slug.
+		 *
+		 * @param string $slug Product slug.
+		 * @return K86_Product_Model|null
+		 */
+		public function find_by_slug( $slug ) {
+
+			return null;
+
+		}
+
+		/**
+		 * Find by SKU.
+		 *
+		 * @param string $sku Product SKU.
+		 * @return K86_Product_Model|null
+		 */
+		public function find_by_sku( $sku ) {
+
+			return null;
+
+		}
+
+		/**
+		 * Get all products.
+		 *
+		 * @return array
+		 */
+		public function all() {
+
+			return array();
+
+		}
+
+		/**
+		 * Pagination.
+		 *
+		 * @param int $page Current page.
+		 * @param int $per_page Items per page.
+		 * @return array
+		 */
+		public function paginate( $page = 1, $per_page = 20 ) {
+
+			return array();
+
+		}
+
+		/**
+		 * Save product.
+		 *
+		 * @param K86_Product_Model $product Product model.
+		 * @return bool|int
+		 */
+		public function save( K86_Product_Model $product ) {
+
+			return false;
+
+		}
+
+		/**
+		 * Delete product.
+		 *
+		 * @param int $id Product ID.
+		 * @return bool
+		 */
+		public function delete( $id ) {
+
+			return false;
+
+		}
+
+	}
+}
