17fc7d6bde8c069f9d9dfcb8598ed90169f5b985
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-23 08:04:26 +0700

    Update repository.php

--- next-core/data/repository.php
+++ next-core/data/repository.php
@@ -11,34 +11,38 @@ defined( 'ABSPATH' ) || exit;
 if ( ! class_exists( 'K86_Product_Repository' ) ) {
 
 	class K86_Product_Repository {
+		/**
+ * Storage.
+ *
+ * @var K86_Product_Storage_Interface
+ */
+protected $storage;
+		public function __construct(
+	K86_Product_Storage_Interface $storage = null
+) {
+
+	$this->storage = $storage
+		? $storage
+		: new K86_Product_Database_Storage();
+
+		}
 
 		public function find( $id ) {
 
-			$data = array(
-				'id' => absint( $id ),
-			);
+	return $this->storage->find( $id );
 
-			$data = apply_filters( 'k86_repository_find', $data, $id );
-
-			return new K86_Product_Model( $data );
 		}
 
 		public function find_by_slug( $slug ) {
 
-			$slug = sanitize_title( $slug );
+	return $this->storage->find_by_slug( $slug );
 
-			do_action( 'k86_repository_find_by_slug', $slug );
-
-			return null;
 		}
 
 		public function find_by_sku( $sku ) {
 
-			$sku = sanitize_text_field( $sku );
+	return $this->storage->find_by_sku( $sku );
 
-			do_action( 'k86_repository_find_by_sku', $sku );
-
-			return null;
 		}
 
 		public function exists( $id ) {
@@ -48,64 +52,27 @@ if ( ! class_exists( 'K86_Product_Repository' ) ) {
 
 		public function all() {
 
-			$products = array();
+	return $this->storage->all();
 
-			return apply_filters(
-				'k86_repository_all',
-				$products
-			);
 		}
 
 		public function paginate( $page = 1, $per_page = 20 ) {
 
-			$result = array(
-				'items'    => array(),
-				'total'    => 0,
-				'page'     => max( 1, absint( $page ) ),
-				'per_page' => max( 1, absint( $per_page ) ),
-			);
+	return $this->storage->paginate( $page, $per_page );
 
-			return apply_filters(
-				'k86_repository_paginate',
-				$result,
-				$page,
-				$per_page
-			);
 		}
+
 
 		public function save( K86_Product_Model $product ) {
 
-			if ( method_exists( $product, 'to_array' ) ) {
-				$data = $product->to_array();
-			} else {
-				$data = array();
-			}
+	return $this->storage->save( $product );
 
-			$data = apply_filters(
-				'k86_before_repository_save',
-				$data,
-				$product
-			);
-
-			do_action(
-				'k86_repository_save',
-				$data,
-				$product
-			);
-
-			return true;
 		}
 
 		public function delete( $id ) {
 
-			$id = absint( $id );
+	return $this->storage->delete( $id );
 
-			do_action(
-				'k86_repository_delete',
-				$id
-			);
-
-			return true;
 		}
 	}
 
