afc1d2fa266b31a893543e338434082c8640246c
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-23 21:34:01 +0700

    Update kernel.php

--- next-core/kernel/kernel.php
+++ next-core/kernel/kernel.php
@@ -1,1 +1,77 @@
+<?php
+/**
+ * K86 Pro - Next Core Kernel
+ *
+ * Central coordinator for the Next Core framework.
+ *
+ * @package K86Pro
+ * @since 1.6.0
+ */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class K86_Kernel {
+
+	/**
+	 * Service Container.
+	 *
+	 * @var K86_Container
+	 */
+	protected $container;
+
+	/**
+	 * Global Registry.
+	 *
+	 * @var K86_Registry
+	 */
+	protected $registry;
+
+	/**
+	 * Engine Manager.
+	 *
+	 * @var K86_Engine_Manager
+	 */
+	protected $engine_manager;
+
+	/**
+	 * Module Registry.
+	 *
+	 * @var K86_Module_Registry
+	 */
+	protected $module_registry;
+
+	/**
+	 * Kernel boot status.
+	 *
+	 * @var bool
+	 */
+	protected $booted = false;
+
+	/**
+	 * Kernel running status.
+	 *
+	 * @var bool
+	 */
+	protected $running = false;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param K86_Container       $container
+	 * @param K86_Registry        $registry
+	 * @param K86_Engine_Manager  $engine_manager
+	 * @param K86_Module_Registry $module_registry
+	 */
+	public function __construct(
+		K86_Container $container,
+		K86_Registry $registry,
+		K86_Engine_Manager $engine_manager,
+		K86_Module_Registry $module_registry
+	) {
+		$this->container       = $container;
+		$this->registry        = $registry;
+		$this->engine_manager  = $engine_manager;
+		$this->module_registry = $module_registry;
+  }
