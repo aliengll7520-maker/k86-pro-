6775925f87f1e06fdfdcac01dc25a2da2c37bb9c
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-23 11:43:18 +0700

    Update return-policy-engine.php

--- next-core/engine/return-policy-engine.php
+++ next-core/engine/return-policy-engine.php
@@ -119,7 +119,113 @@ if ( ! class_exists( 'K86_Return_Policy_Engine' ) ) {
 			);
 
 		}
+	/**
+	 * Set refund support.
+	 *
+	 * @param bool $enabled Refund support.
+	 *
+	 * @return $this
+	 */
+	public function set_refund_support( $enabled ) {
 
+		return $this->register(
+			'refund_support',
+			(bool) $enabled
+		);
+
+	}
+
+	/**
+	 * Check refund support.
+	 *
+	 * @return bool
+	 */
+	public function has_refund_support() {
+
+		return (bool) $this->get(
+			'refund_support',
+			false
+		);
+
+	}
+
+	/**
+	 * Set return shipping fee.
+	 *
+	 * @param float $fee Return shipping fee.
+	 *
+	 * @return $this
+	 */
+	public function set_return_fee( $fee ) {
+
+		return $this->register(
+			'return_fee',
+			max( 0, (float) $fee )
+		);
+
+	}
+
+	/**
+	 * Get return shipping fee.
+	 *
+	 * @return float
+	 */
+	public function get_return_fee() {
+
+		return (float) $this->get(
+			'return_fee',
+			0
+		);
+
+	}
+
+	/**
+	 * Set return note.
+	 *
+	 * @param string $note Return note.
+	 *
+	 * @return $this
+	 */
+	public function set_return_note( $note ) {
+
+		return $this->register(
+			'return_note',
+			sanitize_textarea_field( $note )
+		);
+
+	}
+
+	/**
+	 * Get return note.
+	 *
+	 * @return string
+	 */
+	public function get_return_note() {
+
+		return (string) $this->get(
+			'return_note',
+			''
+		);
+
+	}
+
+	/**
+	 * Get return label.
+	 *
+	 * @return string
+	 */
+	public function get_return_label() {
+
+		if ( ! $this->is_returnable() ) {
+			return __( 'Không hỗ trợ đổi trả', 'k86-pro' );
+		}
+
+		return sprintf(
+			__( 'Đổi trả trong %s', 'k86-pro' ),
+			$this->get_period()
+		);
+
+	}
 	}
 
 }
