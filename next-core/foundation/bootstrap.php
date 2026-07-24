28959f96eff5b0da64e71f0a3fdcd1616bf2b00b
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-24 19:04:51 +0700

    Update bootstrap.php

--- next-core/foundation/bootstrap.php
+++ next-core/foundation/bootstrap.php
@@ -31,21 +31,22 @@ if ( ! class_exists( 'K86_Bootstrap' ) ) {
 			// Register framework services.
 			$provider = new K86_Service_Provider();
 			$provider->register( $container, $manager );
+			$provider->boot();
 
 			// Shared services.
 			$services = array(
 
 				// Core.
-				'container'      => $container,
-				'engine_manager' => $manager,
+				'container'       => $container,
+				'engine_manager'  => $manager,
 
 				// Foundation.
-				'config'         => 'config',
-				'registry'       => 'registry',
-				'helpers'        => 'helpers',
-				'logger'         => 'logger',
-				'event_manager'  => 'event_manager',
-				'error_handler'  => 'error_handler',
+				'config'          => 'config',
+				'registry'        => 'registry',
+				'helpers'         => 'helpers',
+				'logger'          => 'logger',
+				'event_manager'   => 'event_manager',
+				'error_handler'   => 'error_handler',
 
 				// Services.
 				'product_service' => 'product_service',
@@ -83,31 +84,31 @@ if ( ! class_exists( 'K86_Bootstrap' ) ) {
 					}
 				}
 			}
-// Boot Module Registry.
-if ( $registry->has( 'module_registry' ) ) {
 
-	$module_registry = $registry->get( 'module_registry' );
+			// Boot Module Registry.
+			if ( $registry->has( 'module_registry' ) ) {
 
-	if ( method_exists( $module_registry, 'boot' ) ) {
-		$module_registry->boot();
-	}
-}
+				$module_registry = $registry->get( 'module_registry' );
 
-// Boot WordPress Hooks.
-if ( $registry->has( 'wordpress_hooks' ) ) {
+				if ( method_exists( $module_registry, 'boot' ) ) {
+					$module_registry->boot();
+				}
+			}
 
-	$wp_hooks = $registry->get( 'wordpress_hooks' );
+			// Boot WordPress Hooks.
+			if ( $registry->has( 'wordpress_hooks' ) ) {
 
-	if ( method_exists( $wp_hooks, 'boot' ) ) {
-		$wp_hooks->boot();
-	}
-}
+				$wp_hooks = $registry->get( 'wordpress_hooks' );
 
-do_action( 'k86_next_core_booted', $registry );
+				if ( method_exists( $wp_hooks, 'boot' ) ) {
+					$wp_hooks->boot();
+				}
+			}
+
+			do_action( 'k86_next_core_booted', $registry );
+
 			return $registry;
-
 		}
-
 	}
 
 }
