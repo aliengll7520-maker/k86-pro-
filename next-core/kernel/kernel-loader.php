c6827fe1c514bc3ebeb8ddb14651da84585d27ed
aliengll7520-maker <aliengll7520@gmail.com>
2026-07-24 19:08:02 +0700

    Update kernel-loader.php

--- next-core/kernel/kernel-loader.php
+++ next-core/kernel/kernel-loader.php
@@ -8,89 +8,92 @@
  * @since 1.6.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+defined( 'ABSPATH' ) || exit;
 
-class K86_Kernel_Loader {
+if ( ! class_exists( 'K86_Kernel_Loader' ) ) {
 
-	/**
-	 * Kernel instance.
-	 *
-	 * @var K86_Kernel
-	 */
-	protected $kernel;
+	class K86_Kernel_Loader {
 
-	/**
-	 * Constructor.
-	 *
-	 * @param K86_Kernel $kernel Kernel instance.
-	 */
-	public function __construct( K86_Kernel $kernel ) {
-		$this->kernel = $kernel;
-	}
+		/**
+		 * Kernel instance.
+		 *
+		 * @var K86_Kernel
+		 */
+		protected $kernel;
 
-	/**
-	 * Boot the Kernel.
-	 *
-	 * @return void
-	 */
-	public function boot() {
-
-		do_action( 'k86_kernel_loader_before_boot', $this );
-
-		$this->kernel->boot();
-
-		do_action( 'k86_kernel_loader_after_boot', $this );
-  }
-  	/**
-	 * Shutdown the Kernel.
-	 *
-	 * @return void
-	 */
-	public function shutdown() {
-
-		do_action( 'k86_kernel_loader_before_shutdown', $this );
-
-		$this->kernel->shutdown();
-
-		do_action( 'k86_kernel_loader_after_shutdown', $this );
-	}
-
-	/**
-	 * Get Kernel instance.
-	 *
-	 * @return K86_Kernel
-	 */
-	public function getKernel() {
-		return $this->kernel;
-	}
-
-	/**
-	 * Check whether the Kernel has been booted.
-	 *
-	 * @return bool
-	 */
-	public function isBooted() {
-
-		if ( ! $this->kernel ) {
-			return false;
+		/**
+		 * Constructor.
+		 *
+		 * @param K86_Kernel $kernel Kernel instance.
+		 */
+		public function __construct( K86_Kernel $kernel ) {
+			$this->kernel = $kernel;
 		}
 
-		return $this->kernel->isBooted();
-	}
+		/**
+		 * Boot the Kernel.
+		 *
+		 * @return void
+		 */
+		public function boot() {
 
-	/**
-	 * Check whether the Kernel is running.
-	 *
-	 * @return bool
-	 */
-	public function isRunning() {
+			do_action( 'k86_kernel_loader_before_boot', $this );
 
-		if ( ! $this->kernel ) {
-			return false;
+			$this->kernel->boot();
+
+			do_action( 'k86_kernel_loader_after_boot', $this );
 		}
 
-		return $this->kernel->isRunning();
+		/**
+		 * Shutdown the Kernel.
+		 *
+		 * @return void
+		 */
+		public function shutdown() {
+
+			do_action( 'k86_kernel_loader_before_shutdown', $this );
+
+			$this->kernel->shutdown();
+
+			do_action( 'k86_kernel_loader_after_shutdown', $this );
+		}
+
+		/**
+		 * Get Kernel instance.
+		 *
+		 * @return K86_Kernel
+		 */
+		public function get_kernel() {
+			return $this->kernel;
+		}
+
+		/**
+		 * Check whether the Kernel has been booted.
+		 *
+		 * @return bool
+		 */
+		public function is_booted() {
+
+			if ( ! $this->kernel ) {
+				return false;
+			}
+
+			return $this->kernel->isBooted();
+		}
+
+		/**
+		 * Check whether the Kernel is running.
+		 *
+		 * @return bool
+		 */
+		public function is_running() {
+
+			if ( ! $this->kernel ) {
+				return false;
+			}
+
+			return $this->kernel->isRunning();
+		}
 	}
+
 }
